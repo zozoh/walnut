ba752f53200dcf82a620f9cea95d5e16ca8adc1e
Peter Zhang <zozohtnt@gmail.com>
2015-05-09 16:16:42 +0800

    Fix issue #3 提供更灵活的应用模板和占位符机制

--- src/org/nutz/walnut/web/module/AbstractWnModule.java
+++ src/org/nutz/walnut/web/module/AbstractWnModule.java
@@ -1,18 +1,30 @@
 package org.nutz.walnut.web.module;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.lang.Lang;
 import org.nutz.lang.Strings;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
+import org.nutz.walnut.api.box.WnBox;
+import org.nutz.walnut.api.box.WnBoxContext;
 import org.nutz.walnut.api.box.WnBoxService;
 import org.nutz.walnut.api.err.Er;
 import org.nutz.walnut.api.io.MimeMap;
 import org.nutz.walnut.api.io.WnIo;
 import org.nutz.walnut.api.io.WnObj;
+import org.nutz.walnut.api.usr.WnSession;
 import org.nutz.walnut.api.usr.WnSessionService;
 import org.nutz.walnut.api.usr.WnUsrService;
+import org.nutz.walnut.impl.box.Jvms;
 import org.nutz.walnut.util.Wn;
 import org.nutz.walnut.web.WnConfig;
 
 public abstract class AbstractWnModule {
+
+    private static final Log log = Logs.get();
 
     @Inject("refer:conf")
     protected WnConfig conf;
@@ -29,8 +41,80 @@ public abstract class AbstractWnModule {
     @Inject("refer:boxService")
     protected WnBoxService boxes;
 
+    @Inject("java:$conf.getInt('box-alloc-timeout')")
+    protected int allocTimeout;
+
     @Inject("refer:mimes")
     protected MimeMap mimes;
+
+    protected String _run_cmd(String logPrefix, WnSession se, String input, String cmdText) {
+        StringBuilder sbOut = new StringBuilder();
+        StringBuilder sbErr = new StringBuilder();
+        OutputStream out = Lang.ops(sbOut);
+        OutputStream err = Lang.ops(sbErr);
+        InputStream in = null == input ? null : Lang.ins(input);
+
+        _run_cmd(logPrefix, se, cmdText, out, err, in);
+
+        if (sbErr.length() > 0)
+            throw Er.create("e.cmd.error", sbErr);
+
+        return sbOut.toString();
+    }
+
+    protected void _run_cmd(String logPrefix,
+                            WnSession se,
+                            String cmdText,
+                            OutputStream out,
+                            OutputStream err,
+                            InputStream in) {
+        // 得到一个沙箱
+        WnBox box = boxes.alloc(allocTimeout);
+
+        if (log.isDebugEnabled())
+            log.debugf("%sbox:alloc: %s", logPrefix, box.id());
+
+        // 保存到请求属性中，box.onClose 的时候会删除这个属性
+        // req.setAttribute(WnBox.class.getName(), box);
+
+        // 设置沙箱
+        WnBoxContext bc = new WnBoxContext();
+        bc.io = io;
+        bc.me = usrs.check(se.me());
+        bc.session = se;
+        bc.usrService = usrs;
+        bc.sessionService = sess;
+
+        if (log.isDebugEnabled())
+            log.debugf("%sbox:setup: %s", logPrefix, bc);
+        box.setup(bc);
+
+        // 准备回调
+        if (log.isDebugEnabled())
+            log.debugf("%sbox:set stdin/out/err", logPrefix);
+
+        box.setStdin(in);
+        box.setStdout(out);
+        box.setStderr(err);
+
+        // 运行
+        if (log.isDebugEnabled())
+            log.debugf("%sbox:run: %s", logPrefix, cmdText);
+        
+        String[] cmdLines = Jvms.split(cmdText, true, '\n', ';');
+        for (String cmdLine : cmdLines) {
+            box.submit(cmdLine);
+        }
+        box.run();
+
+        // 释放沙箱
+        if (log.isDebugEnabled())
+            log.debugf("%sbox:free: %s", logPrefix, box.id());
+        boxes.free(box);
+
+        if (log.isDebugEnabled())
+            log.debugf("%sbox:done", logPrefix);
+    }
 
     protected WnObj _find_app_home(String appName) {
         String rpath = appName.replace('.', '/');
--- src/org/nutz/walnut/web/module/AppModule.java
+++ src/org/nutz/walnut/web/module/AppModule.java
@@ -8,28 +8,24 @@ import java.net.URLDecoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
+import org.nutz.lang.Lang;
 import org.nutz.lang.Strings;
 import org.nutz.lang.segment.Segment;
 import org.nutz.lang.segment.Segments;
-import org.nutz.log.Log;
-import org.nutz.log.Logs;
+import org.nutz.lang.util.Context;
+import org.nutz.lang.util.NutMap;
 import org.nutz.mvc.View;
 import org.nutz.mvc.annotation.*;
 import org.nutz.mvc.view.JspView;
 import org.nutz.mvc.view.ServerRedirectView;
 import org.nutz.mvc.view.ViewWrapper;
-import org.nutz.walnut.api.box.WnBox;
-import org.nutz.walnut.api.box.WnBoxContext;
 import org.nutz.walnut.api.err.Er;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.api.usr.WnSession;
-import org.nutz.walnut.impl.box.Jvms;
 import org.nutz.walnut.util.Wn;
-import org.nutz.walnut.util.WnContext;
 import org.nutz.walnut.web.bean.WnApp;
 import org.nutz.walnut.web.filter.WnCheckSession;
 import org.nutz.walnut.web.view.WnObjDownloadView;
@@ -38,11 +34,6 @@ import org.nutz.walnut.web.view.WnObjDownloadView;
 @At("/a")
 @Filters(@By(type = WnCheckSession.class))
 public class AppModule extends AbstractWnModule {
-
-    private static final Log log = Logs.get();
-
-    @Inject("java:$conf.getInt('box-alloc-timeout')")
-    private int allocTimeout;
 
     @At("/open/**")
     @Ok("jsp:jsp.app")
@@ -91,8 +82,58 @@ public class AppModule extends AbstractWnModule {
         app.setSession(Wn.WC().checkSE());
         app.setName(appName);
 
+        // 这个是 app 的 JSON 描述
         String appJson = Json.toJson(app, JsonFormat.forLook().setQuoteName(true));
 
+        // 临时设置一下当前目录
+        se.env("PWD", oAppHome.path());
+
+        // 这个是要输出的模板
+        String tmpl;
+
+        // 如果存在 `init_tmpl` 文件，则执行，将其结果作为模板
+        WnObj oInitTmpl = io.fetch(oAppHome, "init_tmpl");
+        if (null != oInitTmpl) {
+            String cmdText = io.readText(oInitTmpl);
+            tmpl = _run_cmd("app-init-tmpl:", se, appJson, cmdText);
+        }
+        // 否则查找静态模板文件
+        else {
+            tmpl = __find_tmpl(appName, oAppHome);
+        }
+
+        // 分析模板
+        Segment seg = Segments.create(tmpl);
+
+        // 如果存在 `init_context` 文件，则执行，将其结果合并到渲染上下文中
+        NutMap map = null;
+        WnObj oInitContext = io.fetch(oAppHome, "init_context");
+        if (null != oInitContext) {
+            String cmdText = io.readText(oInitContext);
+            String contextJson = _run_cmd("app-init-context", se, appJson, cmdText);
+            map = Json.fromJson(NutMap.class, contextJson);
+        }
+
+        // 标题
+        String title = appName;
+        if (null != o)
+            title = o.name() + " : " + title;
+
+        // 填充模板占位符
+        Context c = Lang.context();
+        c.set("title", title);
+        c.set("rs", conf.get("app-rs"));
+        c.set("appName", appName);
+        c.set("app", appJson);
+        c.set("appClass", appName.replace('.', '_').toLowerCase());
+        if (null != map)
+            c.putAll(map);
+
+        // 渲染输出
+        return new ViewWrapper(new JspView("jsp.app"), seg.render(c));
+    }
+
+    private String __find_tmpl(String appName, WnObj oAppHome) {
         // 找到主界面模板
         String tt = "pc"; // 可以是 "pc" 或者 "mobile"
 
@@ -113,22 +154,7 @@ public class AppModule extends AbstractWnModule {
 
         // 读取模板并分析
         String tmpl = io.readText(oTmpl);
-        Segment seg = Segments.create(tmpl);
-
-        // 标题
-        String title = appName;
-        if (null != o)
-            title = o.name() + " : " + title;
-
-        // 填充模板占位符
-        seg.set("title", title);
-        seg.set("rs", conf.get("app-rs"));
-        seg.set("appName", appName);
-        seg.set("app", appJson);
-        seg.set("appClass", appName.replace('.', '_').toLowerCase());
-
-        // 渲染输出
-        return new ViewWrapper(new JspView("jsp.app"), seg.render());
+        return tmpl;
     }
 
     @At("/load/?/**")
@@ -149,61 +175,19 @@ public class AppModule extends AbstractWnModule {
 
         // 得到命令行
         String cmdText = Strings.trim(URLDecoder.decode(req.getQueryString(), "UTF-8"));
-        String[] cmdLines = Jvms.split(cmdText, true, '\n', ';');
 
         // 默认返回的 mime-type 是文本
         if (Strings.isBlank(mimeType))
             mimeType = "text/plain";
         resp.setContentType(mimeType);
 
-        // 得到一个沙箱
-        WnBox box = boxes.alloc(allocTimeout);
-
-        if (log.isDebugEnabled())
-            log.debugf("box:alloc: %s", box.id());
-
-        // 保存到请求属性中，box.onClose 的时候会删除这个属性
-        // req.setAttribute(WnBox.class.getName(), box);
-
-        // 设置沙箱
-        WnContext wc = Wn.WC();
-        WnBoxContext bc = new WnBoxContext();
-        bc.io = io;
-        bc.me = usrs.check(wc.checkMe());
-        bc.session = wc.checkSE();
-        bc.usrService = usrs;
-        bc.sessionService = sess;
-
-        if (log.isDebugEnabled())
-            log.debugf("box:setup: %s", bc);
-        box.setup(bc);
-
-        // 准备回调
-        if (log.isDebugEnabled())
-            log.debug("box:set stdin/out/err");
-
+        // 准备输出
         OutputStream out = new AppRespOutputStreamWrapper(resp, 200);
         OutputStream err = new AppRespOutputStreamWrapper(resp, 500);
 
-        box.setStdin(null); // HTTP GET 方式，不支持沙箱的 stdin
-        box.setStdout(out);
-        box.setStderr(err);
-
         // 运行
-        if (log.isDebugEnabled())
-            log.debugf("box:run: %s", cmdText);
-        for (String cmdLine : cmdLines) {
-            box.submit(cmdLine);
-        }
-        box.run();
-
-        // 释放沙箱
-        if (log.isDebugEnabled())
-            log.debugf("box:free: %s", box.id());
-        boxes.free(box);
-
-        if (log.isDebugEnabled())
-            log.debug("box:done");
+        WnSession se = Wn.WC().checkSE();
+        _run_cmd("", se, cmdText, out, err, null);
     }
 
 }
