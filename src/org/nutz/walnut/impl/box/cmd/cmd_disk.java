3c3df028851e9dcd7e61ce97b69cd408b2dfe2cc
pangwu86 <pangwu86@gmail.com>
2015-06-06 13:09:24 +0800

    add:disk命令,类似ls,返回的时obj对象信息

---
+++ src/org/nutz/walnut/impl/box/cmd/cmd_disk.java
@@ -0,0 +1,55 @@
+package org.nutz.walnut.impl.box.cmd;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.nutz.json.Json;
+import org.nutz.json.JsonFormat;
+import org.nutz.lang.Each;
+import org.nutz.walnut.api.io.WnObj;
+import org.nutz.walnut.impl.box.JvmExecutor;
+import org.nutz.walnut.impl.box.WnSystem;
+import org.nutz.walnut.util.ZParams;
+
+public class cmd_disk extends JvmExecutor {
+
+    @Override
+    public void exec(final WnSystem sys, String[] args) {
+        // 分析参数
+        final ZParams params = ZParams.parse(args, "lhAi");
+        // TODO 搞搞参数...
+
+        // 计算要列出的目录并得到当前目录
+        List<WnObj> list = new LinkedList<WnObj>();
+        WnObj p = evalCandidateObjs(sys, params.vals, list, true);
+
+        // 检查是否候选对象列表为空
+        checkCandidateObjsNoEmpty(args, list);
+
+        // 准备要显示的表格
+        String keys = "";
+        if (params.is("l")) {
+            keys = "md,c,g,len,lm";
+            if (params.is("i"))
+                keys = "id," + keys;
+        }
+        final boolean showHidden = params.is("A");
+        // 先显示文件
+        for (WnObj o : list) {
+            if (o.isFILE()) {
+                sys.out.println(Json.toJson(o, JsonFormat.compact()));
+            }
+        }
+        // 再输出所有的目录
+        for (WnObj o : list) {
+            if (!o.isFILE()) {
+                sys.io.eachChildren(o, null, new Each<WnObj>() {
+                    public void invoke(int index, WnObj child, int length) {
+                        if (!child.isHidden() || showHidden)
+                            sys.out.println(Json.toJson(child, JsonFormat.compact()));
+                    }
+                });
+            }
+        }
+    }
+}
