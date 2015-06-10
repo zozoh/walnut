d06929fcf46e32f7ae2b58f4008f95cfc3ae5e10
pangwu86 <pangwu86@gmail.com>
2015-06-11 04:08:49 +0800

    add:cmd_email

---
+++ ext/org/nutz/walnut/ext/email/MailReceiver.java
@@ -0,0 +1,14 @@
+package org.nutz.walnut.ext.email;
+
+/**
+ * 邮件接收者
+ * 
+ * @author pw
+ *
+ */
+public class MailReceiver {
+
+    public String email;
+
+    public String name;
+}
---
+++ ext/org/nutz/walnut/ext/email/SendConf.java
@@ -0,0 +1,21 @@
+package org.nutz.walnut.ext.email;
+
+/**
+ * 发送配置
+ * 
+ * @author pw
+ *
+ */
+public class SendConf {
+
+    public String host;
+
+    public int port;
+
+    public String account;
+
+    public String alias;
+
+    public String password;
+
+}
