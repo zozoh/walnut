435b0ec1331b5d0142185db362ced892c26e503c
pangwu86 <pangwu86@gmail.com>
2015-06-30 21:52:35 +0800

    nutz已更新

--- src/org/nutz/walnut/api/hook/WnHookContext.java
+++ src/org/nutz/walnut/api/hook/WnHookContext.java
@@ -1,5 +1,7 @@
 package org.nutz.walnut.api.hook;
 
+import org.nutz.lang.stream.VoidInputStream;
+import org.nutz.lang.stream.VoidOutputStream;
 import org.nutz.walnut.api.box.WnBox;
 import org.nutz.walnut.api.box.WnBoxContext;
 import org.nutz.walnut.api.box.WnBoxService;
@@ -33,9 +35,9 @@ public class WnHookContext {
         box.setup(_bc);
 
         // 设置标准输入输出
-        // box.setStderr(new VoidOutputStream());
-        // box.setStdout(new VoidOutputStream());
-        // box.setStdin(new VoidInputStream());
+        box.setStderr(new VoidOutputStream());
+        box.setStdout(new VoidOutputStream());
+        box.setStdin(new VoidInputStream());
 
         // 执行
         box.run(cmdText);
