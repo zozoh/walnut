43b567d6ea18269ca9de61c15c34c352dc3ef61f
wendal <wendal1985@gmail.com>
2015-07-10 16:26:19 +0800

    change: 用ZParams改成jsc

--- src/org/nutz/walnut/impl/box/cmd/cmd_jsc.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_jsc.java
@@ -1,5 +1,7 @@
 package org.nutz.walnut.impl.box.cmd;
 
+import java.util.Map.Entry;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
@@ -8,6 +10,7 @@ import org.nutz.lang.util.NutMap;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.impl.box.JvmExecutor;
 import org.nutz.walnut.impl.box.WnSystem;
+import org.nutz.walnut.util.ZParams;
 
 /**
  * 实验性质执行js文件或直接读取
@@ -19,31 +22,12 @@ public class cmd_jsc extends JvmExecutor {
     public static ScriptEngineManager engineManager =  new ScriptEngineManager();
 
     public void exec(WnSystem sys, String[] args) throws Exception {
-        boolean debug = false;
-        String jsStr = null;
-        String jsFile = null;
-        String engineName = "nashorn";
-        NutMap vars = new NutMap();
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i];
-            switch (arg) {
-            case "-debug":
-                debug = true;
-                break;
-            case "-f":
-                jsFile = args[++i];
-                break;
-            case "-engine":
-                engineName = args[++i];
-                break;
-            case "vars":
-                vars = Json.fromJson(NutMap.class, args[++i]);
-                break;
-            default:
-                jsStr = arg;
-                break;
-            }
-        }
+        ZParams params = ZParams.parse(args, "^debug$");
+        boolean debug = params.is("debug");
+        String jsStr = params.vals.length > 0 ? params.vals[0] : null;
+        String jsFile = params.get("f");
+        String engineName = params.get("engine", "nashorn");
+        NutMap vars = Json.fromJson(NutMap.class, params.get("vars", "{}"));
         if (jsFile != null) {
             WnObj tmp = sys.io.check(null, jsFile);
             jsStr = sys.io.readText(tmp);
@@ -59,10 +43,10 @@ public class cmd_jsc extends JvmExecutor {
         }
         
         // 过渡一下
-        NutMap walnut = new NutMap();
-        walnut.put("vars", vars);
-        walnut.put("sys", sys);
-        engine.put("walnut", walnut);
+        for (Entry<String, Object> en : vars.entrySet()) {
+            engine.put(en.getKey(), en.getValue());
+        }
+        engine.put("sys", sys);
         if (debug)
             sys.out.println("js=" + jsStr + "\n");
         Object obj = engine.eval(jsStr);
