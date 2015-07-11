85eb62f08bde54450ab6cd54145d4dd47204f6d6
wendal <wendal1985@gmail.com>
2015-07-11 23:57:09 +0800

    change: EscapeCloseOutputStream和EscapeCloseOutputStream改成用FilterXXXStream实现

--- src/org/nutz/walnut/impl/box/EscapeCloseOutputStream.java
+++ src/org/nutz/walnut/impl/box/EscapeCloseOutputStream.java
@@ -1,46 +1,15 @@
 package org.nutz.walnut.impl.box;
 
+import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class EscapeCloseOutputStream extends OutputStream {
-
-    private OutputStream ops;
+public class EscapeCloseOutputStream extends FilterOutputStream {
 
     public EscapeCloseOutputStream(OutputStream ops) {
-        this.ops = ops;
-    }
-
-    public void write(int b) throws IOException {
-        ops.write(b);
-    }
-
-    public int hashCode() {
-        return ops.hashCode();
-    }
-
-    public void write(byte[] b) throws IOException {
-        ops.write(b);
-    }
-
-    public void write(byte[] b, int off, int len) throws IOException {
-        ops.write(b, off, len);
-    }
-
-    public boolean equals(Object obj) {
-        return ops.equals(obj);
-    }
-
-    public void flush() throws IOException {
-        ops.flush();
+        super(ops);
     }
 
     public void close() throws IOException {
-        // ops.close();
     }
-
-    public String toString() {
-        return ops.toString();
-    }
-
 }
