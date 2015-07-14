ed76cf59c8760d690df5db2dda0f4400aa080e65
Peter Zhang <zozohtnt@gmail.com>
2015-07-15 00:15:31 +0800

    修改了 cmd_obj， 支持像表格一样显示对象信息。比如 `obj * -t "race,nm,c,g" -bhsi`，详细请 `man obj` 获得更多帮助

--- src/org/nutz/walnut/impl/box/TextTable.java
+++ src/org/nutz/walnut/impl/box/TextTable.java
@@ -17,6 +17,10 @@ public class TextTable {
 
     private List<String[]> rows;
 
+    private int cellSpacing;
+
+    private boolean showBorder;
+
     public TextTable(int colnb) {
         this.colnb = colnb;
 
@@ -27,10 +31,52 @@ public class TextTable {
         Arrays.fill(this.cell_align_left, true);
 
         this.rows = new LinkedList<String[]>();
+        this.cellSpacing = 1;
+        this.showBorder = false;
+    }
+
+    public int getCellSpacing() {
+        return cellSpacing;
+    }
+
+    public void setCellSpacing(int cellSpacing) {
+        this.cellSpacing = cellSpacing;
+    }
+
+    public boolean isShowBorder() {
+        return showBorder;
+    }
+
+    public void setShowBorder(boolean showBorder) {
+        this.showBorder = showBorder;
     }
 
     public void alignRigth(int colIndex) {
         cell_align_left[colIndex] = false;
+    }
+
+    public void addHr() {
+        rows.add(null);
+    }
+
+    public void addRow(String[] col) {
+        String[] row = new String[colnb];
+        int sz = col.length;
+        int i = 0;
+        // 从头填充
+        for (String str : col) {
+            if (i >= sz)
+                break;
+            row[i] = Strings.sNull(str, "");
+            cell_width[i] = Math.max(cell_width[i], row[i].length());
+            i++;
+        }
+        // 填充不足
+        for (; i < colnb; i++) {
+            row[i] = "";
+        }
+        // 记录行
+        rows.add(row);
     }
 
     public void addRow(Collection<String> col) {
@@ -55,19 +101,40 @@ public class TextTable {
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
+
+        int spacing = showBorder ? (int) Math.ceil((double) cellSpacing / 2.0) : cellSpacing;
+
         for (String[] row : rows) {
-            int last = row.length - 1;
-            for (int i = 0; i < last; i++) {
-                String cell = row[i];
-                if (cell_align_left[i]) {
-                    sb.append(Strings.alignLeft(cell, cell_width[i], ' '));
-                } else {
-                    sb.append(Strings.alignRight(cell, cell_width[i], ' '));
+            // 输出分隔线
+            if (null == row) {
+                int last = colnb - 1;
+                for (int i = 0; i < last; i++) {
+                    sb.append(Strings.dup('-', spacing + cell_width[i]));
+                    if (showBorder) {
+                        sb.append('+').append(Strings.dup('-', spacing));
+                    }
                 }
-                sb.append(' ');
+                // 加上最后一列
+                sb.append(Strings.dup('-', cell_width[last])).append('\n');
             }
-            // 加上最后一列
-            sb.append(row[last]).append('\n');
+            // 输出普通行
+            else {
+                int last = row.length - 1;
+                for (int i = 0; i < last; i++) {
+                    String cell = row[i];
+                    if (cell_align_left[i]) {
+                        sb.append(Strings.alignLeft(cell, cell_width[i], ' '));
+                    } else {
+                        sb.append(Strings.alignRight(cell, cell_width[i], ' '));
+                    }
+                    sb.append(Strings.dup(' ', spacing));
+                    if (showBorder) {
+                        sb.append('|').append(Strings.dup(' ', spacing));
+                    }
+                }
+                // 加上最后一列
+                sb.append(row[last]).append('\n');
+            }
         }
         return sb.toString();
     }
--- src/org/nutz/walnut/impl/box/cmd/cmd_obj.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_obj.java
@@ -1,5 +1,6 @@
 package org.nutz.walnut.impl.box.cmd;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -8,18 +9,21 @@ import java.util.regex.Pattern;
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
 import org.nutz.lang.Lang;
+import org.nutz.lang.Strings;
 import org.nutz.lang.Times;
 import org.nutz.lang.util.NutMap;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.impl.box.JvmExecutor;
+import org.nutz.walnut.impl.box.TextTable;
 import org.nutz.walnut.impl.box.WnSystem;
+import org.nutz.walnut.impl.io.WnBean;
 import org.nutz.walnut.util.ZParams;
 
 public class cmd_obj extends JvmExecutor {
 
     @Override
     public void exec(WnSystem sys, String[] args) {
-        ZParams params = ZParams.parse(args, "cV");
+        ZParams params = ZParams.parse(args, "iocnqhbsl");
 
         // 首先获取对象
         // 计算要列出的要处理的对象
@@ -30,6 +34,7 @@ public class cmd_obj extends JvmExecutor {
         checkCandidateObjsNoEmpty(args, list);
 
         // 一次处理所有对象
+        List<NutMap> outs = new ArrayList<NutMap>(list.size());
         for (WnObj o : list) {
             // 更新对象
             if (params.has("u")) {
@@ -48,7 +53,7 @@ public class cmd_obj extends JvmExecutor {
                                 en.setValue(Times.D(str));
                             }
                         }
-                        // 毫米数
+                        // 毫秒数
                         else if (s.startsWith("$ms:")) {
                             String str = s.substring("$ms:".length());
                             if ("now".equals(str)) {
@@ -60,40 +65,94 @@ public class cmd_obj extends JvmExecutor {
                     }
                 }
                 sys.io.appendMeta(o, map);
-
             }
-            // 显示对象的值
-            else if (params.has("e")) {
-                Pattern p = Pattern.compile(params.get("e"));
+            // ................................................
+            // 记录输出
+            // 显示对象某几个值
+            if (params.has("e")) {
+                String regex = params.check("e");
+                boolean not = false;
+                if (regex.startsWith("!")) {
+                    not = true;
+                    regex = regex.substring(1);
+                }
+                Pattern p = Pattern.compile(regex);
                 NutMap map = new NutMap();
                 for (String key : o.keySet()) {
                     if (p.matcher(key).matches()) {
+                        if (!not)
+                            map.put(key, o.get(key));
+                    } else if (not) {
                         map.put(key, o.get(key));
                     }
                 }
-                // 只有一个值的话，则显示值
-                if (map.size() == 1) {
-                    sys.out.println(map.values().iterator().next().toString());
-                }
-                // 紧凑显示
-                else if (params.is("c")) {
-                    sys.out.println(Json.toJson(map, JsonFormat.compact()));
-                }
-                // 只显示值
-                else if (params.is("V")) {
-                    for (Object v : map.values()) {
-                        sys.out.print(v.toString());
-                    }
-                    sys.out.println();
-                }
-                // 否则显示全部匹配
-                else {
-                    sys.out.println(Json.toJson(map));
-                }
+                outs.add(map);
             }
             // 显示对象全部的值
             else {
-                sys.out.println(Json.toJson(o));
+                if (o instanceof WnBean)
+                    outs.add((NutMap) o);
+                else
+                    outs.add(o.toMap(null));
+            }
+        }
+        // 当没有更新，或者强制输出的时候，执行输出
+        if (outs.size() > 0 && (params.is("o") || !params.has("u"))) {
+            // 按表格输出
+            if (params.has("t")) {
+                String sCols = params.get("t");
+                String[] aCols = Strings.splitIgnoreBlank(sCols);
+                if (params.is("i")) {
+                    aCols = Lang.arrayFirst("#", aCols);
+                }
+
+                // 准备输出表
+                TextTable tt = new TextTable(aCols.length);
+                if (params.is("b")) {
+                    tt.setShowBorder(true);
+                } else {
+                    tt.setCellSpacing(2);
+                }
+                // 加标题
+                if (params.is("h")) {
+                    tt.addRow(aCols);
+                    tt.addHr();
+                }
+                // 主体
+                int i = params.getInt("ibase", 0);
+                for (NutMap map : outs) {
+                    List<String> cells = new ArrayList<String>(aCols.length);
+                    for (String key : aCols) {
+                        if ("#".equals(key)) {
+                            cells.add("" + (i++));
+                            continue;
+                        }
+                        Object v = map.get(key);
+                        cells.add(v == null ? null : v.toString());
+                    }
+                    tt.addRow(cells);
+                }
+                // 尾部
+                if (params.is("s")) {
+                    tt.addHr();
+                }
+                // 输出
+                sys.out.print(tt.toString());
+                if (params.is("s")) {
+                    sys.out.printlnf("total %d items", outs.size());
+                }
+            }
+            // 用 Json 的方法输出
+            else {
+                JsonFormat fmt = params.is("c") ? JsonFormat.compact() : JsonFormat.forLook();
+                fmt.setIgnoreNull(!params.is("n")).setQuoteName(params.is("q"));
+                String json;
+                if (params.is("l") || outs.size() > 1) {
+                    json = Json.toJson(outs, fmt);
+                } else {
+                    json = Json.toJson(outs.get(0), fmt);
+                }
+                sys.out.println(json);
             }
         }
     }
