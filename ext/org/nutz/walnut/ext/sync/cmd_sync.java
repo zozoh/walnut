e90c57b3318ab9cd73d84f0c592a54c70fb7193c
wendal <wendal1985@gmail.com>
2015-10-27 03:40:49 +0800

    add: sync的设计文档草稿

---
+++ ext/org/nutz/walnut/ext/sync/cmd_sync.java
@@ -0,0 +1,17 @@
+package org.nutz.walnut.ext.sync;
+
+import org.nutz.walnut.impl.box.JvmExecutor;
+import org.nutz.walnut.impl.box.WnSystem;
+
+/**
+ * 
+ * @author wendal(wendal1985@gmail.com)
+ *
+ */
+public class cmd_sync extends JvmExecutor {
+
+    public void exec(WnSystem sys, String[] args) throws Exception {
+        
+    }
+
+}
