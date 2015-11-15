3026a2e9aa1d6c8155060ba7f0e3ca4a48a5375c
pangwu86 <pangwu86@gmail.com>
2015-11-15 23:05:42 +0800

    ..

--- conf/ioc/io.js
+++ conf/ioc/io.js
@@ -1,51 +1,51 @@
 var ioc = {
-    mimes : {
-	type : 'org.nutz.walnut.impl.io.MimeMapImpl',
-	args : [ {
-	    type : 'org.nutz.ioc.impl.PropertiesProxy',
-	    args : [ {
-		java : '$conf.get("mime","mime.properties")'
-	    } ]
-	} ]
-    },
-    tree : {
-	type : 'org.nutz.walnut.impl.io.mongo.MongoWnTree',
-	args : [ {
-	    java : '$mongoDB.getCollection("obj")'
-	}, {
-	    java : '$conf.rootTreeNode'
-	}, {
-	    refer : 'mimes'
-	} ]
-    },
-    store : {
-	type : 'org.nutz.walnut.impl.io.WnStoreImpl',
-	fields : {
-	    buckets : {
-		type : 'org.nutz.walnut.impl.io.mongo.MongoLocalBucketManager',
+	mimes : {
+		type : 'org.nutz.walnut.impl.io.MimeMapImpl',
 		args : [ {
-		    java : '$conf.bucketHome'
-		}, {
-		    java : '$mongoDB.getCollection("bucket")'
+			type : 'org.nutz.ioc.impl.PropertiesProxy',
+			args : [ {
+				java : '$conf.get("mime","mime.properties")'
+			} ]
 		} ]
-	    },
-	    handles : {
-		type : 'org.nutz.walnut.impl.io.handle.WnHandleManagerImpl'
-	    }
+	},
+	tree : {
+		type : 'org.nutz.walnut.impl.io.mongo.MongoWnTree',
+		args : [ {
+			java : '$mongoDB.getCollection("obj")'
+		}, {
+			java : '$conf.rootTreeNode'
+		}, {
+			refer : 'mimes'
+		} ]
+	},
+	store : {
+		type : 'org.nutz.walnut.impl.io.WnStoreImpl',
+		fields : {
+			buckets : {
+				type : 'org.nutz.walnut.impl.io.mongo.MongoLocalBucketManager',
+				args : [ {
+					java : '$conf.bucketHome'
+				}, {
+					java : '$mongoDB.getCollection("bucket")'
+				} ]
+			},
+			handles : {
+				type : 'org.nutz.walnut.impl.io.handle.WnHandleManagerImpl'
+			}
+		}
+	},
+	io : {
+		type : 'org.nutz.walnut.impl.io.WnIoImpl',
+		fields : {
+			mimes : {
+				refer : 'mimes'
+			},
+			tree : {
+				refer : 'tree'
+			},
+			store : {
+				refer : 'store'
+			}
+		}
 	}
-    },
-    io : {
-	type : 'org.nutz.walnut.impl.io.WnIoImpl',
-	fields : {
-	    mimes : {
-		refer : 'mimes'
-	    },
-	    tree : {
-		refer : 'tree'
-	    },
-	    store : {
-		refer : 'store'
-	    }
-	}
-    }
 }
