ad272faac531c265da29115ce1f396032a19841b
wendal chen <wendal1985@gmail.com>
2016-01-29 18:45:10 +0800

    add: adduser命令

---
+++ src/org/nutz/walnut/impl/box/cmd/cmd_adduser.java
@@ -0,0 +1,17 @@
+package org.nutz.walnut.impl.box.cmd;
+
+import org.nutz.lang.Strings;
+import org.nutz.walnut.impl.box.JvmExecutor;
+import org.nutz.walnut.impl.box.WnSystem;
+
+public class cmd_adduser extends JvmExecutor {
+
+	public void exec(WnSystem sys, String[] args) throws Exception {
+		if (args.length != 1)
+            return;
+		String name = Strings.trim(args[0]);
+		String password = args.length > 1 ? Strings.trim(args[1]) : "123456";
+		sys.usrService.create(name, password);
+	}
+
+}
