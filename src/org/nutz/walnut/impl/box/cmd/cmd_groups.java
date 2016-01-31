447ab31056318a57bd82bef13f8123692dfa8340
Peter Zhang <zozohtnt@gmail.com>
2016-02-01 03:37:08 +0800

    格式化一下代码

--- src/org/nutz/walnut/impl/box/cmd/cmd_groups.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_groups.java
@@ -8,19 +8,20 @@ import org.nutz.walnut.util.Wn;
 
 /**
  * 列出用户所在的组
+ * 
  * @author wendal
  *
  */
 public class cmd_groups extends JvmExecutor {
 
-	public void exec(WnSystem sys, String[] args) throws Exception {
-		WnObj grpDir = sys.io.check(null, "/sys/grp");
-		WnUsr usr = args.length > 0 ? sys.usrService.check(args[0]) : sys.me;
-		sys.io.each(Wn.Q.pid(grpDir.id()), (index,child,length)->{
-			WnObj p = sys.io.fetch(child, "people/"+usr.id());
-			if (p != null && p.getInt("role", 0) == 1)
-				sys.out.println(child.name());
-		});
-	}
+    public void exec(WnSystem sys, String[] args) throws Exception {
+        WnObj grpDir = sys.io.check(null, "/sys/grp");
+        WnUsr usr = args.length > 0 ? sys.usrService.check(args[0]) : sys.me;
+        sys.io.each(Wn.Q.pid(grpDir.id()), (index, child, length) -> {
+            WnObj p = sys.io.fetch(child, "people/" + usr.id());
+            if (p != null && p.getInt("role", 0) == 1)
+                sys.out.println(child.name());
+        });
+    }
 
 }
