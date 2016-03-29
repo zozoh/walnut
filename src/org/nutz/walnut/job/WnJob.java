f1e9005e56e6cc726b6c4e251f3782404aa8c647
wendal chen <wendal1985@gmail.com>
2016-03-29 13:14:02 +0800

    fix: WnJob执行任务时没有触发hook

--- src/org/nutz/walnut/job/WnJob.java
+++ src/org/nutz/walnut/job/WnJob.java
@@ -7,6 +7,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
+import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.lang.Lang;
 import org.nutz.lang.Strings;
@@ -14,6 +15,9 @@ import org.nutz.lang.random.R;
 import org.nutz.lang.util.NutMap;
 import org.nutz.quartz.Quartz;
 import org.nutz.quartz.QzEach;
+import org.nutz.walnut.api.box.WnBoxContext;
+import org.nutz.walnut.api.hook.WnHookContext;
+import org.nutz.walnut.api.hook.WnHookService;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.api.io.WnQuery;
 import org.nutz.walnut.api.io.WnRace;
@@ -22,31 +26,33 @@ import org.nutz.walnut.api.usr.WnUsr;
 import org.nutz.walnut.util.Wn;
 import org.nutz.walnut.util.WnRun;
 
-@IocBean(create="init", depose="depose")
+@IocBean(create = "init", depose = "depose")
 public class WnJob extends WnRun implements Callable<Object> {
-	
+
     protected static ThreadPoolExecutor es = (ThreadPoolExecutor) Executors.newFixedThreadPool(64);
     public static String root = "/sys/job";
     protected String tmpRoot = "/sys/job/tmp";
     public static WnJob me;
+    @Inject
+    protected WnHookService hookService;
 
     public boolean isRunning() {
         return es != null && !es.isShutdown();
     }
-    
+
     public void depose() {
-    	if (es != null)
-    		es.shutdown();
+        if (es != null)
+            es.shutdown();
     }
-    
+
     public String toString() {
-    	return es.toString();
+        return es.toString();
     }
-    
+
     public WnJob() {
-		me = this;
-	}
-    
+        me = this;
+    }
+
     public void init() {
         io.createIfNoExists(null, root, WnRace.DIR);
         io.createIfNoExists(null, tmpRoot, WnRace.DIR);
@@ -58,8 +64,8 @@ public class WnJob extends WnRun implements Callable<Object> {
         String pid = io.check(null, root).id();
         WnQuery query = new WnQuery();
         query.setv("pid", pid);
-        //query.limit(1);
-        //query.sortBy("job_ava", 1);
+        // query.limit(1);
+        // query.sortBy("job_ava", 1);
         query.setv("job_ava", new NutMap().setv("$lt", now.getTime()));
         List<WnObj> list = io.query(query);
         if (list == null || list.isEmpty())
@@ -67,7 +73,7 @@ public class WnJob extends WnRun implements Callable<Object> {
         WnObj jobDir = list.get(0);
         String cron = jobDir.getString("job_cron");
         if (Strings.isBlank(cron)) {
-            io.appendMeta(jobDir, "job_ava:"+(now.getTime()+24*60*60*1000L));
+            io.appendMeta(jobDir, "job_ava:" + (now.getTime() + 24 * 60 * 60 * 1000L));
             return jobDir;
         }
         final Calendar calendar = Calendar.getInstance();
@@ -75,10 +81,10 @@ public class WnJob extends WnRun implements Callable<Object> {
         if (calendar.get(Calendar.HOUR_OF_DAY) == 23 && calendar.get(Calendar.MINUTE) > 55) {
             calendar.set(Calendar.MINUTE, 56);
             calendar.add(Calendar.DAY_OF_YEAR, 1);
-            io.appendMeta(jobDir, "job_ava:"+calendar.getTimeInMillis());
+            io.appendMeta(jobDir, "job_ava:" + calendar.getTimeInMillis());
         } else {
             calendar.set(Calendar.MINUTE, 56);
-            io.appendMeta(jobDir, "job_ava:"+calendar.getTimeInMillis());
+            io.appendMeta(jobDir, "job_ava:" + calendar.getTimeInMillis());
         }
         // TODO 如果cron是非法的,就挂了
         Quartz quartz = Quartz.NEW(cron);
@@ -86,12 +92,12 @@ public class WnJob extends WnRun implements Callable<Object> {
             return null;
         }
         int MIN_TNT = 3; // 3分钟间隔
-        Integer[] jobs = new Integer[24*60/MIN_TNT];
+        Integer[] jobs = new Integer[24 * 60 / MIN_TNT];
         quartz.each(jobs, new QzEach<Integer>() {
             public void invoke(Integer[] array, int index) throws Exception {
-                calendar.set(Calendar.HOUR_OF_DAY, (index*MIN_TNT)/60);
-                calendar.set(Calendar.MINUTE, (index*MIN_TNT)%60);
-                WnObj _jobDir = io.create(null, tmpRoot+"/"+R.UU32(), WnRace.DIR);
+                calendar.set(Calendar.HOUR_OF_DAY, (index * MIN_TNT) / 60);
+                calendar.set(Calendar.MINUTE, (index * MIN_TNT) % 60);
+                WnObj _jobDir = io.create(null, tmpRoot + "/" + R.UU32(), WnRace.DIR);
                 WnObj _jobCmd = io.create(_jobDir, "cmd", WnRace.FILE);
                 // TODO 咋拷贝文件夹呢?
             }
@@ -120,19 +126,37 @@ public class WnJob extends WnRun implements Callable<Object> {
 
         public Object call() throws Exception {
             try {
-                io.appendMeta(jobDir, new NutMap().setv("job_start", System.currentTimeMillis()).setv("job_st", "run"));
+                io.appendMeta(jobDir,
+                              new NutMap().setv("job_start", System.currentTimeMillis())
+                                          .setv("job_st", "run"));
                 WnObj cmdFile = io.fetch(jobDir, "cmd");
                 if (cmdFile != null) {
                     String cmdText = io.readText(cmdFile);
                     WnUsr usr = usrs.fetch(jobDir.getString("job_user"));
                     if (usr != null) {
-                    	WnSession se = sess.create(usr);
-                    	Wn.WC().me(usr.name(), jobDir.getString("job_group", usr.name()));
-                        exec("job-"+jobDir.getString("job_name", "_")+" ", se, "", cmdText);
+                        WnSession se = sess.create(usr);
+                        Wn.WC().me(usr.name(), jobDir.getString("job_group", usr.name()));
+                        WnBoxContext bc = new WnBoxContext();
+                        bc.io = io;
+                        bc.me = usr;
+                        bc.session = se;
+                        bc.usrService = usrs;
+                        bc.sessionService = sess;
+                        WnHookContext hc = new WnHookContext(boxes, bc);
+                        hc.io = io;
+                        hc.me = usr;
+                        hc.se = se;
+                        hc.service = hookService;
+
+                        Wn.WC().setHookContext(hc);
+                        exec("job-" + jobDir.getString("job_name", "_") + " ", se, "", cmdText);
                     }
                 }
-            } finally {
-                io.appendMeta(jobDir, new NutMap().setv("job_end", System.currentTimeMillis()).setv("job_st", "done"));
+            }
+            finally {
+                io.appendMeta(jobDir,
+                              new NutMap().setv("job_end", System.currentTimeMillis())
+                                          .setv("job_st", "done"));
             }
             return null;
         }
