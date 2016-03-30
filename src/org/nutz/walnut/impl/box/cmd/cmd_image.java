1562afb6f12630cd74bd104306df24a9cb4f2504
Peter Zhang <zozohtnt@gmail.com>
2016-03-30 19:44:07 +0800

    增强 image 命令，让其能看到图片更多信息

--- src/org/nutz/walnut/impl/box/cmd/cmd_image.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_image.java
@@ -22,6 +22,52 @@ public class cmd_image extends JvmExecutor {
         BufferedImage bimg = Images.read(sys.io.getInputStream(imgObj, 0));
         imgInfo.setv("width", bimg.getWidth());
         imgInfo.setv("height", bimg.getHeight());
+        imgInfo.setv("type", bimg.getType());
+
+        switch (bimg.getType()) {
+        case BufferedImage.TYPE_INT_RGB:
+            imgInfo.setv("typeName", "INT_RGB");
+            break;
+        case BufferedImage.TYPE_INT_ARGB:
+            imgInfo.setv("typeName", "INT_ARGB");
+            break;
+        case BufferedImage.TYPE_INT_ARGB_PRE:
+            imgInfo.setv("typeName", "INT_ARGB_PRE");
+            break;
+        case BufferedImage.TYPE_INT_BGR:
+            imgInfo.setv("typeName", "INT_BGR");
+            break;
+        case BufferedImage.TYPE_3BYTE_BGR:
+            imgInfo.setv("typeName", "3BYTE_BGR");
+            break;
+        case BufferedImage.TYPE_4BYTE_ABGR:
+            imgInfo.setv("typeName", "4BYTE_ABGR");
+            break;
+        case BufferedImage.TYPE_4BYTE_ABGR_PRE:
+            imgInfo.setv("typeName", "4BYTE_ABGR_PRE");
+            break;
+        case BufferedImage.TYPE_BYTE_GRAY:
+            imgInfo.setv("typeName", "BYTE_GRAY");
+            break;
+        case BufferedImage.TYPE_USHORT_GRAY:
+            imgInfo.setv("typeName", "USHORT_GRAY");
+            break;
+        case BufferedImage.TYPE_BYTE_BINARY:
+            imgInfo.setv("typeName", "BYTE_BINARY");
+            break;
+        case BufferedImage.TYPE_BYTE_INDEXED:
+            imgInfo.setv("typeName", "BYTE_INDEXED");
+            break;
+        case BufferedImage.TYPE_USHORT_565_RGB:
+            imgInfo.setv("typeName", "USHORT_565_RGB");
+            break;
+        case BufferedImage.TYPE_USHORT_555_RGB:
+            imgInfo.setv("typeName", "USHORT_555_RGB");
+            break;
+        default:
+            imgInfo.setv("typeName", "???");
+        }
+
         return imgInfo;
     }
 
