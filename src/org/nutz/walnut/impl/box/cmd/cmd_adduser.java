aadd7340fdc0214a5e103388c1cd2ffd8abeea88
wendal chen <wendal1985@gmail.com>
2016-05-19 22:25:28 +0800

    fix: add user方法总是不能设置密码

--- src/org/nutz/walnut/impl/box/cmd/cmd_adduser.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_adduser.java
@@ -7,7 +7,7 @@ import org.nutz.walnut.impl.box.WnSystem;
 public class cmd_adduser extends JvmExecutor {
 
 	public void exec(WnSystem sys, String[] args) throws Exception {
-		if (args.length != 1)
+		if (args.length < 1)
             return;
 		String name = Strings.trim(args[0]);
 		String password = args.length > 1 ? Strings.trim(args[1]) : "123456";
