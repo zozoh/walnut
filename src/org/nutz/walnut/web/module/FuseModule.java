81cb96619bb9ceb898007e4aa5a9d000ae87267d
wendal chen <wendal1985@gmail.com>
2016-06-09 22:00:31 +0800

    add: fuse支持chmod, 修正readlink的bug

--- src/org/nutz/walnut/web/module/FuseModule.java
+++ src/org/nutz/walnut/web/module/FuseModule.java
@@ -208,7 +208,44 @@ public class FuseModule extends AbstractWnModule {
     	WnObj obj = _obj();
     	if (obj == null)
     		return null;
-    	return obj.link();
+    	return Wn.real(obj, io).path();
+    }
+
+    @At
+    @Ok("raw")
+    public int open(@Param("path")String path, @Param("flags")int flags) {
+        /*
+        #define O_RDONLY    0x0000
+        #define O_WRONLY    0x0001
+        #define O_RDWR      0x0002
+        #define O_ACCMODE   0x0003
+
+        #define O_BINARY    0x0004  
+        #define O_TEXT      0x0008  
+        #define O_NOINHERIT 0x0080  
+
+        #define O_CREAT     0x0100  
+        #define O_EXCL      0x0200
+        #define O_NOCTTY    0x0400
+        #define O_TRUNC     0x0800
+        #define O_APPEND    0x1000
+        #define O_NONBLOCK  0x2000
+        */
+        return 0; 
+    }
+    
+    @At
+    @Ok("raw")
+    public int release(@Param("path")String path, @Param("fh")int fh) {
+        
+        return 0;
+    }
+    
+    @At
+    @Ok("void")
+    public void chmod(@Param("path")String path, @Param("mode")int mode) {
+       _obj().mode(mode);
+       io.set(_obj(), "^md$");
     }
 
     public WnObj _obj() {
