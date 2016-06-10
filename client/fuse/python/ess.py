250e109007881c09b937aef893d9dcf326dbb70f
wendal <wendal1985@gmail.com>
2016-06-10 09:41:06 +0800

    update: 让fusepy的init传入conn对象,从而修改fuse的特性,支持open时直接truncate

--- client/fuse/python/ess.py
+++ client/fuse/python/ess.py
@@ -39,6 +39,15 @@ class ESS(LoggingMixIn, Operations):
             raise FuseOSError(EBUSY)
         return resp
 
+    def init(self, root, conn):
+        conn = conn.contents
+        print(dir(conn))
+        print(conn.proto_major)
+        print(conn.proto_minor)
+        print(conn.capable)
+        print(conn.want)
+        conn.want |= 8
+
     def chmod(self, path, mode):
         with closing(self._get("chmod", dict(path=path,mode=mode))) as resp :
             if resp.status_code != 200 :
@@ -111,7 +120,7 @@ class ESS(LoggingMixIn, Operations):
             return 0
 
     def truncate(self, path, length, fh=None):
-        with closing(self._get("truncate", dict(path=path, length=length))) as resp :
+        with closing(self._get("truncate", dict(path=path, length=length, fh=fh))) as resp :
             pass
         return 0
 
@@ -142,12 +151,13 @@ if __name__ == '__main__':
     MOUNT_POINT = argv[4]
     import  logging
     logging.basicConfig(filename='ess.log',level=logging.INFO)
-    fuse = FUSE(ESS(argv[1], argv[2], int(argv[3])), argv[4], foreground=1, debug=0, 
-					entry_timeout=5, attr_timeout=5
+    fuse = FUSE(ESS(argv[1], argv[2], int(argv[3])), argv[4], foreground=1, debug=0
+					#,entry_timeout=5, attr_timeout=5
 					, big_writes=True
 					, kernel_cache=True
 					, auto_cache=True
 					#,large_read=True
+					#, flags=8
 					)
     
     
