6bcc483bc6610fa113298f3c4c91022cc7380699
pangwu86 <pangwu86@gmail.com>
2016-07-17 16:06:10 +0800

    add:全屏幕方法

--- ROOT/rs/core/js/nutz/zutil.js
+++ ROOT/rs/core/js/nutz/zutil.js
@@ -9,6 +9,31 @@
     var INDENT_BY = "    ";
 
     var zUtil = {
+        // 全屏幕
+        toggleFullScreen() {
+            if (!document.fullscreenElement &&    // alternative standard method
+                !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
+                if (document.documentElement.requestFullscreen) {
+                    document.documentElement.requestFullscreen();
+                } else if (document.documentElement.msRequestFullscreen) {
+                    document.documentElement.msRequestFullscreen();
+                } else if (document.documentElement.mozRequestFullScreen) {
+                    document.documentElement.mozRequestFullScreen();
+                } else if (document.documentElement.webkitRequestFullscreen) {
+                    document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+                }
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                }
+            }
+        },
         //.............................................
         // 提供 AMD/CMD 支持功能
         defineModule : function(mdName, mdObj){
