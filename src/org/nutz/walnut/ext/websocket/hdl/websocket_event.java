1d96524d147862965b12d4ad86ac2065db77457d
wendal chen <wendal1985@gmail.com>
2016-09-28 10:06:19 +0800

    change: websocket_event的参数项合并到顶层

--- src/org/nutz/walnut/ext/websocket/hdl/websocket_event.java
+++ src/org/nutz/walnut/ext/websocket/hdl/websocket_event.java
@@ -6,6 +6,7 @@ import javax.websocket.Session;
 
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
+import org.nutz.lang.Lang;
 import org.nutz.lang.Strings;
 import org.nutz.lang.random.R;
 import org.nutz.lang.util.NutMap;
@@ -21,18 +22,19 @@ import org.nutz.walnut.util.ZParams;
 
 public class websocket_event implements JvmHdl {
 
-    @SuppressWarnings("unchecked")
     public void invoke(WnSystem sys, JvmHdlContext hc) {
         ZParams params = ZParams.parse(hc.args, null);
         Session session = WnWebSocket.get(params.val_check(0));
         if (session == null)
             return;
         String event = params.val_check(1);
-        Map<Object, Object> map = null;
+        Map<String, Object> map = null;
         if (params.vals.length > 2) {
-            map = Json.fromJson(Map.class, params.val(2));
+            map = Lang.map(params.val(2));
         }
-        NutMap re = new NutMap("event", event).setv("params", map);
+        NutMap re = new NutMap("event", event);
+        if (params != null)
+            re.putAll(map);
         String callback = params.get("callback");
         if (!Strings.isBlank(callback)) {
             String id = R.UU32();
