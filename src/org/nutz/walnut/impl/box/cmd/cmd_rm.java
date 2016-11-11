f3af0239c4f4f05fddda5b6e5a42f3025df3f66e
Peter Zhang <zozohtnt@gmail.com>
2016-11-12 00:22:19 +0800

    为 rm 增加 limit 参数

--- src/org/nutz/walnut/impl/box/cmd/cmd_rm.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_rm.java
@@ -1,6 +1,8 @@
 package org.nutz.walnut.impl.box.cmd;
 
 import org.nutz.lang.Each;
+import org.nutz.lang.Stopwatch;
+import org.nutz.lang.Times;
 import org.nutz.lang.util.Disks;
 import org.nutz.walnut.api.err.Er;
 import org.nutz.walnut.api.io.WnObj;
@@ -14,9 +16,13 @@ public class cmd_rm extends JvmExecutor {
 
     @Override
     public void exec(WnSystem sys, String[] args) {
-        ZParams params = ZParams.parse(args, "rfv");
+        ZParams params = ZParams.parse(args, "rfvI");
         boolean isV = params.is("v");
         boolean isR = params.is("r");
+        boolean isI = params.is("I");
+
+        // limit
+        int limit = params.getInt("limit", 0);
 
         // 参数错误
         if (params.vals.length < 1) {
@@ -29,6 +35,8 @@ public class cmd_rm extends JvmExecutor {
 
         // 循环每个参数
         WnQuery q = Wn.Q.pid(oCurrent);
+        if (limit > 0)
+            q.limit(limit);
         for (String str : params.vals) {
             // 修改通配符
             str = str.replace("*", ".*");
@@ -37,11 +45,22 @@ public class cmd_rm extends JvmExecutor {
             q.setv("nm", str);
 
             // 挨个查一下，然后删除
+            final int[] count = new int[1];
+            Stopwatch sw = Stopwatch.begin();
             sys.io.each(q, new Each<WnObj>() {
                 public void invoke(int index, WnObj o, int length) {
-                    _do_delete(sys, isV, isR, base, o);
+                    _do_delete(sys, isV, isR, isI, count[0], base, o);
+                    count[0]++;
                 }
             });
+            sw.stop();
+
+            // 最后打印结束
+            if (isV) {
+                long du = sw.getDuration();
+                String ts = Times.sT((int) du / 1000);
+                sys.out.printlnf("%d obj deleted in %s (%sms)", count[0], ts, du);
+            }
         }
 
     }
@@ -49,18 +68,24 @@ public class cmd_rm extends JvmExecutor {
     protected void _do_delete(final WnSystem sys,
                               final boolean isV,
                               final boolean isR,
+                              final boolean isI,
+                              int index,
                               final String base,
                               WnObj o) {
         // 打印
         if (isV) {
-            sys.out.printlnf(Disks.getRelativePath(base, o.path()));
+            if (isI) {
+                sys.out.printlnf("%d. %s", index, Disks.getRelativePath(base, o.path()));
+            } else {
+                sys.out.println(Disks.getRelativePath(base, o.path()));
+            }
         }
 
         // 递归
         if (!o.isFILE() && isR) {
             sys.io.each(Wn.Q.pid(o.id()), new Each<WnObj>() {
                 public void invoke(int index, WnObj child, int length) {
-                    _do_delete(sys, isV, isR, base, child);
+                    _do_delete(sys, isV, isR, isI, index, base, child);
                 }
             });
         }
