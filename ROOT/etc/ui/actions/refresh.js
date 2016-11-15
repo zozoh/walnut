8fa4a04819e8fc5a1fa34f06fbf828025956fc51
pangwu86 <pangwu86@gmail.com>
2016-11-15 17:39:33 +0800

    fixed #99

--- ROOT/etc/ui/actions/refresh.js
+++ ROOT/etc/ui/actions/refresh.js
@@ -10,7 +10,7 @@
 		if(_.isFunction(UI.refresh)){
 			UI.refresh();
 		}
-		else if(UI.browser()){
+		else if(_.isFunction(UI.browser)){
 			UI.browser().refresh();
 		}
 		else if(UI.parent && _.isFunction(UI.parent.refresh)){
