76df91d2a2542572b26da1a1f2b995509ec80e90
Peter Zhang <zozohtnt@gmail.com>
2016-12-12 16:49:09 +0800

    做点代码小修改

--- src/org/nutz/walnut/web/module/HttpApiModule.java
+++ src/org/nutz/walnut/web/module/HttpApiModule.java
@@ -16,7 +16,6 @@ import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.lang.Lang;
 import org.nutz.lang.Streams;
 import org.nutz.lang.Strings;
-import org.nutz.lang.meta.Pair;
 import org.nutz.lang.tmpl.Tmpl;
 import org.nutz.lang.util.NutMap;
 import org.nutz.log.Log;
@@ -53,7 +52,8 @@ public class HttpApiModule extends AbstractWnModule {
     public void do_api(String usr,
                        String api,
                        final HttpServletRequest req,
-                       final HttpServletResponse resp) throws IOException {
+                       final HttpServletResponse resp)
+            throws IOException {
         final WnUsr u;
         final WnObj oHome;
         final WnObj oApi;
@@ -63,32 +63,19 @@ public class HttpApiModule extends AbstractWnModule {
             if (log.isInfoEnabled())
                 log.infof("httpAPI(%s): /%s/%s", req.getRemoteAddr(), usr, api);
 
+            // 得到用户和主目录
             u = usrs.check(usr);
             String homePath = Strings.sBlank(u.home(), "/home/" + u.name());
             oHome = io.check(null, homePath);
-            WnObj oApiHome = Wn.WC().su(u, new Proton<WnObj>() {
-                protected WnObj exec() {
-                    return io.createIfNoExists(oHome, ".regapi/api", WnRace.DIR);
-                }
-            });
-            WnObj o = io.fetch(oApiHome, api);
-            // 逐级寻找 default
-            if (null == o) {
-                WnObj p = oApiHome;
-                String[] ss = Strings.splitIgnoreBlank(api, "/");
-                int i = 0;
-                while (p != null) {
-                    WnObj oDft = io.fetch(p, "_default");
-                    if (null != oDft)
-                        o = oDft;
-                    p = io.fetch(p, ss, i, i + 1);
-                }
-            }
-            if (null == o) {
+
+            // 找到 API 对象
+            oApi = io.fetch(oHome, Wn.appendPath(".regapi/api", api));
+
+            // 如果没有这个 API 文件，就 404 吧
+            if (null == oApi) {
                 respCode = 404;
                 throw Er.create("e.api.nofound", api);
             }
-            oApi = o;
 
             // 将当前线程切换到指定的用户
             WnContext wc = Wn.WC();
@@ -102,9 +89,19 @@ public class HttpApiModule extends AbstractWnModule {
                 wc.copyCookieItems(req, copyCookieNames);
             }
 
+            // 准备临时目录
+            final WnObj oTmp = Wn.WC().su(u, new Proton<WnObj>() {
+                protected WnObj exec() {
+                    return io.createIfNoExists(oHome, ".regapi/tmp", WnRace.DIR);
+                }
+            });
+
+            // 生成请求对象
+            WnObj oReq = __gen_req_obj(req, u, oApi, oTmp);
+
             // 执行 API 文件
             try {
-                _do_api(req, resp, oHome, se, u, oApi);
+                _do_api(oReq, resp, oHome, se, u, oApi);
             }
             // 确保退出登录
             finally {
@@ -124,85 +121,16 @@ public class HttpApiModule extends AbstractWnModule {
 
     }
 
-    private void _do_api(HttpServletRequest req,
+    private void _do_api(WnObj oReq,
                          HttpServletResponse resp,
                          final WnObj oHome,
                          WnSession se,
                          WnUsr u,
-                         WnObj oApi) throws IOException {
-
-        // 读取输入
-        final WnObj oTmp = Wn.WC().su(u, new Proton<WnObj>() {
-            protected WnObj exec() {
-                return io.createIfNoExists(oHome, ".regapi/tmp", WnRace.DIR);
-            }
-        });
-
-        // 创建临时文件以便保存请求的内容
-        WnObj oReq = Wn.WC().su(u, new Proton<WnObj>() {
-            protected WnObj exec() {
-                return io.create(oTmp, "${id}", WnRace.FILE);
-            }
-        });
-        Enumeration<String> hnms = req.getHeaderNames();
-        NutMap map = new NutMap();
-
-        // 保存 http 参数
-        map.setv("http-usr", u.name()).setv("http-api", oApi.name());
-
-        map.setv("http-protocol", req.getProtocol().toLowerCase());
-        map.setv("http-method", req.getMethod().toUpperCase());
-        map.setv("http-uri", req.getRequestURI());
-        map.setv("http-url", req.getRequestURL());
-        map.setv("http-remote-addr", req.getRemoteAddr());
-        map.setv("http-remote-host", req.getRemoteHost());
-        map.setv("http-remote-port", req.getRemotePort());
+                         WnObj oApi)
+            throws IOException {
 
         // 记录是否客户端设定了响应的 ContentType
-        String mimeType = null;
-
-        // 保存 QueryString，同时，看看有没必要更改 mime-type
-        String qs = req.getQueryString();
-        map.setv("http-qs", qs);
-        if (!Strings.isBlank(qs)) {
-            qs = URLDecoder.decode(qs, "UTF-8");
-            String[] ss = Strings.splitIgnoreBlank(qs, "[&]");
-            for (String s : ss) {
-                Pair<String> pair = Pair.create(s);
-                int pos = s.indexOf('=');
-                if (pos > 0) {
-                    String nm = s.substring(0, pos);
-                    String val = s.substring(pos + 1);
-                    map.setv("http-qs-" + nm, val);
-                } else {
-                    map.setv("http-qs-" + s, "");
-                }
-
-                // 如果客户端声明的是 mimeType
-                if ("mime".equals(pair.getName())) {
-                    mimeType = pair.getValue();
-                }
-            }
-        }
-
-        // 保存请求头
-        while (hnms.hasMoreElements()) {
-            String hnm = hnms.nextElement();
-            String hval = req.getHeader(hnm);
-            map.setv("http-header-" + hnm.toUpperCase(), hval);
-        }
-
-        // 更新头信息
-        io.appendMeta(oReq, map);
-
-        // 保存请求体
-        InputStream ins = req.getInputStream();
-        OutputStream ops = io.getOutputStream(oReq, 0);
-        Streams.writeAndClose(ops, ins);
-
-        // 将请求的对象设置一下清除标志（缓存 30 分钟)
-        oReq.expireTime(System.currentTimeMillis() + 1800000L);
-        io.set(oReq, "^expi$");
+        String mimeType = oReq.getString("http-qs-mime");
 
         // 解析命令
         String cmdPattern = io.readText(oApi);
@@ -234,6 +162,72 @@ public class HttpApiModule extends AbstractWnModule {
         }
     }
 
+    private WnObj __gen_req_obj(HttpServletRequest req, WnUsr u, WnObj oApi, final WnObj oTmp)
+            throws UnsupportedEncodingException, IOException {
+        // 创建临时文件以便保存请求的内容
+        WnObj oReq = Wn.WC().su(u, new Proton<WnObj>() {
+            protected WnObj exec() {
+                return io.create(oTmp, "${id}", WnRace.FILE);
+            }
+        });
+        Enumeration<String> hnms = req.getHeaderNames();
+        NutMap map = new NutMap();
+
+        // 保存 http 参数
+        map.setv("http-usr", u.name()).setv("http-api", oApi.name());
+
+        map.setv("http-protocol", req.getProtocol().toLowerCase());
+        map.setv("http-method", req.getMethod().toUpperCase());
+        map.setv("http-uri", req.getRequestURI());
+        map.setv("http-url", req.getRequestURL());
+        map.setv("http-remote-addr", req.getRemoteAddr());
+        map.setv("http-remote-host", req.getRemoteHost());
+        map.setv("http-remote-port", req.getRemotePort());
+
+        // 保存 QueryString，同时，看看有没必要更改 mime-type
+        String qs = req.getQueryString();
+        map.setv("http-qs", qs);
+        if (!Strings.isBlank(qs)) {
+            // 解码
+            qs = URLDecoder.decode(qs, "UTF-8");
+            // 分析每个请求参数
+            String[] ss = Strings.splitIgnoreBlank(qs, "[&]");
+            for (String s : ss) {
+                int pos = s.indexOf('=');
+                // 有值
+                if (pos > 0) {
+                    String nm = s.substring(0, pos);
+                    String val = s.substring(pos + 1);
+                    map.setv("http-qs-" + nm, val);
+                }
+                // 没值的用空串表示
+                else {
+                    map.setv("http-qs-" + s, "");
+                }
+            }
+        }
+
+        // 保存请求头
+        while (hnms.hasMoreElements()) {
+            String hnm = hnms.nextElement();
+            String hval = req.getHeader(hnm);
+            map.setv("http-header-" + hnm.toUpperCase(), hval);
+        }
+
+        // 更新头信息
+        io.appendMeta(oReq, map);
+
+        // 保存请求体
+        InputStream ins = req.getInputStream();
+        OutputStream ops = io.getOutputStream(oReq, 0);
+        Streams.writeAndClose(ops, ins);
+
+        // 将请求的对象设置一下清除标志（缓存 30 分钟)
+        oReq.expireTime(System.currentTimeMillis() + 1800000L);
+        io.set(oReq, "^expi$");
+        return oReq;
+    }
+
     private void _do_redirect(HttpServletResponse resp, String cmdText, WnSession se, WnUsr u)
             throws IOException {
         StringBuilder sbOut = new StringBuilder();
@@ -261,7 +255,8 @@ public class HttpApiModule extends AbstractWnModule {
                              HttpServletResponse resp,
                              String cmdText,
                              WnSession se,
-                             WnUsr u) throws UnsupportedEncodingException {
+                             WnUsr u)
+            throws UnsupportedEncodingException {
         // 执行命令
         WnBox box = boxes.alloc(0);
 
