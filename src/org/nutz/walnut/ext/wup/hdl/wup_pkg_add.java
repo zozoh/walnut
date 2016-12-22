fd9a7f0939778d963a899de70f093f22111b2162
wendal chen <wendal1985@gmail.com>
2016-12-22 22:58:47 +0800

    add: wup pkg add命令支持批量添加

--- src/org/nutz/walnut/ext/wup/hdl/wup_pkg_add.java
+++ src/org/nutz/walnut/ext/wup/hdl/wup_pkg_add.java
@@ -1,12 +1,18 @@
 package org.nutz.walnut.ext.wup.hdl;
 
-import org.nutz.lang.Strings;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.nutz.json.Json;
+import org.nutz.lang.util.NutMap;
+import org.nutz.walnut.api.io.WalkMode;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.api.io.WnRace;
 import org.nutz.walnut.impl.box.JvmHdl;
 import org.nutz.walnut.impl.box.JvmHdlContext;
 import org.nutz.walnut.impl.box.WnSystem;
 import org.nutz.walnut.util.Wn;
+import org.nutz.walnut.util.ZParams;
 
 /**
  * 添加一个更新包
@@ -16,28 +22,39 @@ import org.nutz.walnut.util.Wn;
 public class wup_pkg_add implements JvmHdl {
 
     public void invoke(WnSystem sys, JvmHdlContext hc) {
-        String name = hc.params.get("name");
-        String version = hc.params.get("version");
-        String source = hc.params.vals[0];
-        source = Wn.normalizeFullPath(source, sys);
-        if (!source.endsWith(".tgz")) {
-            sys.err.println("must be tgz file");
-            return;
+        List<NutMap> list = new ArrayList<>();
+        for (String path : hc.params.vals) {
+            path = Wn.normalizeFullPath(path, sys);
+            WnObj _src = sys.io.check(null, path);
+            sys.io.walk(_src, (wobj) -> {
+                String source = wobj.path();
+                if (!source.endsWith(".tgz")) {
+                    return;
+                }
+                String[] tmp = wobj.name().split("-", 2);
+                if (tmp.length != 2 || tmp[0].isEmpty() || tmp[1].length() < 4)
+                    return;
+                String name = tmp[0];
+                String version = tmp[1].substring(0, tmp[1].length() - 4);
+                
+                String dst = Wn.normalizePath("~/wup/pkgs/"+name+"/"+version+".tgz", sys);
+                WnObj obj = sys.io.createIfNoExists(null, dst, WnRace.FILE);
+                sys.io.writeAndClose(obj, sys.io.getInputStream(wobj, 0));
+                NutMap re = new NutMap("name", name).setv("version", version);
+                list.add(re);
+            }, WalkMode.LEAF_ONLY);
         }
-        WnObj src = sys.io.check(null, source);
-        if (Strings.isBlank(name) || Strings.isBlank(version)) {
-            String[] tmp = src.name().split("-", 2);
-            if (Strings.isBlank(name))
-                name = tmp[0];
-            if (Strings.isBlank(version)) {
-                version = tmp[1].substring(0, tmp[1].length() - 4);
+        sys.out.println(Json.toJson(list));
+        if (hc.params.has("u")) {
+            String[] updates = hc.params.get("u").split(",");
+            for (NutMap re : list) {
+                for (String update : updates) {
+                    if (update.equals(re.get("name"))) {
+                        
+                    }
+                }
             }
         }
-        
-        sys.out.printf("name=%s version=%s source=%s\r\n", name, version, source);
-        String path = Wn.normalizePath("~/wup/pkgs/"+name+"/"+version+".tgz", sys);
-        WnObj obj = sys.io.createIfNoExists(null, path, WnRace.FILE);
-        sys.io.writeAndClose(obj, sys.io.getInputStream(src, 0));
     }
 
 }
