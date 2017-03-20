79da5a7a04d98faa01031084cd5b118a07607193
Peter Zhang <zozohtnt@gmail.com>
2017-03-21 01:18:24 +0800

    修正一些小错误

---
+++ ROOT/etc/ui/editors/edit_weixin_mp.js
@@ -0,0 +1,8 @@
+({
+	key : "edit_weixin_mp",
+	text : "WeixinMPEditor",
+	actions : [ "@::wx_add", "@::wx_del", "@::wx_save"],
+	outline : false,
+	uiType : "app/wn.weixin.mp/weixin_mp",
+	uiConf : {}
+})
