06eef0699c79b4ca09a48155e7b71c91fb630717
pangwu86 <pangwu86@gmail.com>
2017-05-09 13:22:07 +0800

    update:zip支持类似obj的match

--- src/org/nutz/walnut/impl/box/cmd/cmd_zip.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_zip.java
@@ -2,15 +2,22 @@ package org.nutz.walnut.impl.box.cmd;
 
 import java.io.BufferedInputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import org.nutz.json.Json;
+import org.nutz.lang.Lang;
+import org.nutz.lang.Strings;
+import org.nutz.lang.util.NutMap;
 import org.nutz.walnut.api.io.WnObj;
+import org.nutz.walnut.api.io.WnQuery;
 import org.nutz.walnut.api.io.WnRace;
 import org.nutz.walnut.impl.box.JvmExecutor;
 import org.nutz.walnut.impl.box.WnSystem;
 import org.nutz.walnut.util.Wn;
+import org.nutz.walnut.util.WnPager;
 import org.nutz.walnut.util.ZParams;
 import org.nutz.web.Webs.Err;
 
@@ -41,38 +48,87 @@ public class cmd_zip extends JvmExecutor {
 
         // 目标文件
         destObj = sys.io.createIfNoExists(null, destPath, WnRace.FILE);
+
         // 计算要列出的要处理的对象
-        List<WnObj> srclist = evalCandidateObjsNoEmpty(sys, srcArr, 0);
-        try {
-            OutputStream out = sys.io.getOutputStream(destObj, 0);
-            ZipOutputStream zipOut = new ZipOutputStream(out);
-            String entryName = null;
+        List<WnObj> srclist = new ArrayList<WnObj>();
+        if (params.has("match")) {
+            WnPager wp = new WnPager(params);
+            for (String srcPath : srcArr) {
+                String dirPath = Wn.normalizeFullPath(srcPath, sys);
+                WnObj dirObj = sys.io.fetch(null, dirPath);
+                srclist.addAll(queryByMatch(sys, params, dirObj, wp));
+            }
+        } else {
+            srclist = evalCandidateObjsNoEmpty(sys, srcArr, 0);
+        }
+        if (params.is("list")) {
             for (int i = 0; i < srclist.size(); i++) {
                 WnObj srcObj = srclist.get(i);
-                if (destObj.id().equals(srcObj.id())) {
-                    continue;
-                }
-                entryName = srcObj.name();
-                // 创建Zip条目
-                ZipEntry entry = new ZipEntry(entryName);
-                zipOut.putNextEntry(entry);
-
-                BufferedInputStream bis = new BufferedInputStream(sys.io.getInputStream(srcObj, 0));
-
-                byte[] b = new byte[1024];
-                int count = 0;
-                while ((count = bis.read(b, 0, 1024)) != -1) {
-                    zipOut.write(b, 0, count);
-                }
-                bis.close();
-                zipOut.closeEntry();
+                sys.out.printlnf("%s", srcObj.name());
             }
-            zipOut.flush();
-            zipOut.close();
+        } else {
+            try {
+                OutputStream out = sys.io.getOutputStream(destObj, 0);
+                ZipOutputStream zipOut = new ZipOutputStream(out);
+                String entryName = null;
+                for (int i = 0; i < srclist.size(); i++) {
+                    WnObj srcObj = srclist.get(i);
+                    if (destObj.id().equals(srcObj.id())) {
+                        continue;
+                    }
+                    entryName = srcObj.name();
+                    // 创建Zip条目
+                    ZipEntry entry = new ZipEntry(entryName);
+                    zipOut.putNextEntry(entry);
+
+                    BufferedInputStream bis = new BufferedInputStream(sys.io.getInputStream(srcObj,
+                                                                                            0));
+
+                    byte[] b = new byte[1024];
+                    int count = 0;
+                    while ((count = bis.read(b, 0, 1024)) != -1) {
+                        zipOut.write(b, 0, count);
+                    }
+                    bis.close();
+                    zipOut.closeEntry();
+                }
+                zipOut.flush();
+                zipOut.close();
+            }
+            catch (Exception e) {
+                throw Err.create(e, "e.cmd.zip.err");
+            }
         }
-        catch (Exception e) {
-            throw Err.create(e, "e.cmd.zip.err");
+    }
+
+    private List<WnObj> queryByMatch(WnSystem sys, ZParams params, WnObj oP, WnPager wp) {
+        String json = params.get("match", "{}");
+        WnQuery q = new WnQuery();
+        // 条件是"或"
+        if (Strings.isQuoteBy(json, '[', ']')) {
+            List<NutMap> ors = Json.fromJsonAsList(NutMap.class, json);
+            q.addAll(ors);
         }
+        // 条件是"与"
+        else {
+            q.add(Lang.map(json));
+        }
+
+        // 如果指定了父对象 ...
+        if (null != oP)
+            q.setv("pid", oP.id());
+
+        q.setv("d0", "home").setv("d1", sys.me.group());
+
+        // 最大不能超过10w
+        if (wp.limit <= 0) {
+            wp.limit = Math.max(wp.limit, 100000);
+        }
+
+        // 设置分页信息
+        wp.setupQuery(sys, q);
+
+        return sys.io.query(q);
     }
 
 }
