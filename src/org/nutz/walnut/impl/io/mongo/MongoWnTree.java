7fb6e446a689ed294feecd62a51529366e1dd8c9
wendal chen <wendal1985@gmail.com>
2017-06-06 11:49:59 +0800

    fix: each方法内如果时间很长,会导致cursor被服务器drop掉.

--- src/org/nutz/walnut/impl/io/mongo/MongoWnTree.java
+++ src/org/nutz/walnut/impl/io/mongo/MongoWnTree.java
@@ -14,6 +14,7 @@ import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.api.io.WnQuery;
 import org.nutz.walnut.impl.io.AbstractWnTree;
 
+import com.mongodb.Bytes;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 
@@ -69,6 +70,7 @@ public class MongoWnTree extends AbstractWnTree {
         DBCursor cu = co.find(qDoc);
 
         try {
+            cu.addOption(Bytes.QUERYOPTION_NOTIMEOUT);
             int i = 0;
             int n = 0;
             WnMongos.setup_paging(cu, q);
