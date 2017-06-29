568e5166a5514d7845d7668def15f1f5ca9b6657
pangwu86 <pangwu86@gmail.com>
2017-06-29 15:52:27 +0800

    update:sidebar支持mini模式

--- ROOT/rs/core/js/ui/obrowser/obrowser_chute.js
+++ ROOT/rs/core/js/ui/obrowser/obrowser_chute.js
@@ -1,109 +1,94 @@
-(function($z){
-$z.declare([
-    'zui',
-    'wn/util',
-    'ui/obrowser/support/browser__methods'
-], function(ZUI, Wn, BrowserMethods){
+(function ($z) {
+    $z.declare([
+        'zui',
+        'wn/util',
+        'ui/obrowser/support/browser__methods'
+    ], function (ZUI, Wn, BrowserMethods) {
 //==============================================
-var html = `
+        var html = `
 <div class="ui-arena obrowser-chute ui-clr" ui-fitparent="yes">
-    <div class="chute-scroller" doutline>
-        <div class="chute-nav" ui-gasket="sidebar"></div>
-        <div class="chute-outline outline-con">I am outline</div>
-        <div class="chute-scroller-btn chute-show-outline"><i class="fa fa-caret-right"></i></div>
-        <div class="chute-scroller-btn chute-show-nav"><i class="fa fa-caret-left"></i></div>
+    <div class="chute-menu chute-mode-toggle">
+        <i class="fa"></i>
     </div>
+    <div class="chute-nav" ui-gasket="sidebar"></div>
 </div>`;
 //==============================================
-return ZUI.def("ui.obrowser_chute", {
-    dom  : html,
-    //..............................................
-    init : function(){
-        BrowserMethods(this);
-    },
-    //..............................................
-    events : {
-        "click .chute-show-nav" : function(e){
-            this.hideOutline();
-        },
-        "click .chute-show-outline" : function(e){
-            this.showOutline();
-        }
-    },
-    //..............................................
-    redraw : function() {
-        var UI = this;
-        UI.refresh(function(){
-            UI.defer_report("sidebar");
-        });
-        return ["sidebar"];
-    },
-    //..............................................
-    refresh : function(callback) {
-        var UI  = this;
-        var opt = UI.browser().options.sidebar;
-
-        // 显示加载中
-        UI.showLoading();
-
-        // 已经有了 UI 那么就更新
-        if(UI.gasket.sidebar) {
-            UI.gasket.sidebar.refresh(function(){
-                UI.hideLoading();
-                UI.gasket.sidebar.update(UI.__obj, UI.__asetup);
-                $z.doCallback(callback, [this], UI);
-            });
-        }
-        // 否则重新建立
-        else {
-            seajs.use(opt.uiType, function(SubUI){
-                new SubUI(_.extend({}, opt.uiConf, {
-                    parent : UI,
-                    gasketName : "sidebar"
-                })).render(function(){
-                    UI.hideLoading();
-                    this.update(UI.__obj, UI.__asetup);
-                    $z.doCallback(callback, [this], UI);
+        return ZUI.def("ui.obrowser_chute", {
+            dom: html,
+            //..............................................
+            init: function () {
+                BrowserMethods(this);
+            },
+            //..............................................
+            events: {
+                "click .chute-mode-toggle": function (e) {
+                    var $i = $(e.currentTarget).find('i');
+                    $i.toggleClass('fa-angle-double-left').toggleClass('fa-angle-double-right');
+                    this.browser().toogleChuteMode();
+                }
+            },
+            //..............................................
+            redraw: function () {
+                var UI = this;
+                UI.refresh(function () {
+                    UI.defer_report("sidebar");
                 });
-            });
-        }
-    },
-    //..............................................
-    showOutline : function(){
-        var jS = this.arena.find(".chute-scroller").attr("outline", "yes");
-        return jS.children(".chute-outline");
-    },
-    hideOutline : function(){
-        this.arena.find(".chute-scroller").attr("outline", "no");
-    },
-    removeOutline : function(){
-        this.arena.find(".chute-scroller").removeAttr("outline");
-    },
-    isShowOutline : function(){
-        return this.arena.find(".chute-scroller").attr("outline") == "yes";
-    },
-    //..............................................
-    update : function(o, asetup){
-        this.__obj    = o;
-        this.__asetup = asetup;
-        this.gasket.sidebar.update(o, asetup);        
-    },
-    //..............................................
-    resize : function(){
-        var UI = this;
-        var jS = UI.arena.children(".chute-scroller");
-        var jbN = jS.children(".chute-show-nav");
-        var jbO = jS.children(".chute-show-outline");
-        var W = UI.arena.width();
+                return ["sidebar"];
+            },
+            //..............................................
+            refresh: function (callback) {
+                var UI = this;
+                var opt = UI.browser().options.sidebar;
 
-        jbN.css("left", W);
-        jbO.css("right", W);
+                // 显示加载中
+                UI.showLoading();
 
-    }
-    //..............................................
-});
+                // 已经有了 UI 那么就更新
+                if (UI.gasket.sidebar) {
+                    UI.gasket.sidebar.refresh(function () {
+                        UI.hideLoading();
+                        UI.gasket.sidebar.update(UI.__obj, UI.__asetup);
+                        $z.doCallback(callback, [this], UI);
+                    });
+                }
+                // 否则重新建立
+                else {
+                    seajs.use(opt.uiType, function (SubUI) {
+                        new SubUI(_.extend({}, opt.uiConf, {
+                            parent: UI,
+                            gasketName: "sidebar"
+                        })).render(function () {
+                            UI.hideLoading();
+                            this.update(UI.__obj, UI.__asetup);
+                            $z.doCallback(callback, [this], UI);
+                        });
+                    });
+                }
+
+                // 根据cookie设定显示模式
+                var cmode = Cookies.get('chute-mode') || "normal";
+                if (cmode == 'normal') {
+                    UI.arena.find('.chute-mode-toggle i').addClass('fa-angle-double-left')
+                }
+                if (cmode == 'mini') {
+                    UI.arena.find('.chute-mode-toggle i').addClass('fa-angle-double-right')
+                }
+            },
+            //..............................................
+            update: function (o, asetup) {
+                this.__obj = o;
+                this.__asetup = asetup;
+                this.gasket.sidebar.update(o, asetup);
+
+            },
+            //..............................................
+            resize: function () {
+                var UI = this;
+            }
+            //..............................................
+        });
 //==================================================
-});
+    });
 })(window.NutzUtil);
 
 
