912010a848f3d691ee5a3cc2f8d08126af348c7d
Peter Zhang <zozohtnt@gmail.com>
2017-07-06 02:36:17 +0800

    增强 cmd_random

--- src/org/nutz/walnut/impl/box/cmd/cmd_random.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_random.java
@@ -1,5 +1,6 @@
 package org.nutz.walnut.impl.box.cmd;
 
+import org.nutz.lang.Strings;
 import org.nutz.lang.random.R;
 import org.nutz.walnut.impl.box.JvmExecutor;
 import org.nutz.walnut.impl.box.WnSystem;
@@ -11,20 +12,34 @@ public class cmd_random extends JvmExecutor {
     public void exec(WnSystem sys, String[] args) throws Exception {
 
         ZParams params = ZParams.parse(args, "n");
-
-        // 生成几位的随机数，默认 4
-        int nb = params.getInt("s", 4);
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < nb; i++) {
-            sb.append(R.random(0, 9));
-        }
 
-        if (params.is("n"))
-            sb.append('\n');
+        // 固定数量模式
+        if (params.has("s")) {
+            // 生成几位的随机数，默认 4
+            int nb = params.getInt("s", 4);
+            for (int i = 0; i < nb; i++) {
+                sb.append(R.random(0, 9));
+            }
+        }
+        // 整数范围
+        else if (params.vals.length > 0) {
+            String[] ss = Strings.splitIgnoreBlank(params.val(0));
+            int min = Integer.parseInt(ss[0]);
+            int max = Integer.parseInt(ss[1]);
+            sb.append(R.random(min, max));
+        }
+        // 默认是一个浮点数
+        else {
+            sb.append(R.get().nextFloat());
+        }
 
         // 输出
         sys.out.print(sb);
+
+        // 输出空行
+        if (params.is("n"))
+            sys.out.println();
 
     }
 
