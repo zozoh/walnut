2b5db2a7c36740a940095988dd651fe36b3ff4b7
wendal chen <wendal1985@gmail.com>
2017-07-07 10:41:45 +0800

    update: 整理一下ftp命令的代码

--- src/org/nutz/walnut/ext/ftp/hdl/ftp_ls.java
+++ src/org/nutz/walnut/ext/ftp/hdl/ftp_ls.java
@@ -3,6 +3,7 @@ package org.nutz.walnut.ext.ftp.hdl;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.nutz.walnut.ext.ftp.FtpConfig;
+import org.nutz.walnut.ext.ftp.FtpUtil;
 import org.nutz.walnut.impl.box.JvmHdlContext;
 import org.nutz.walnut.impl.box.WnSystem;
 
@@ -11,7 +12,7 @@ public class ftp_ls extends ftp_xxx {
     protected boolean _invoke(WnSystem sys, JvmHdlContext hc, FTPClient client, FtpConfig conf)
             throws Throwable {
         // TODO 输出ls那样的格式
-        String path = ftpPath(conf, hc.params.val_check(0));
+        String path = FtpUtil.ftpPath(conf, hc.params.val_check(0));
         if (!client.changeWorkingDirectory(path)) {
             sys.err.print("not such dir " + path);
             return false;
