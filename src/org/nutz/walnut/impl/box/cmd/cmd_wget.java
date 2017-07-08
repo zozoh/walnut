8004fcb07403153fc727597f03bfaf157c396f56
Peter Zhang <zozohtnt@gmail.com>
2017-07-08 21:29:29 +0800

    重构一下 cmd_wget 让其支持输出到任意目录

--- src/org/nutz/walnut/impl/box/cmd/cmd_wget.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_wget.java
@@ -1,58 +1,56 @@
 package org.nutz.walnut.impl.box.cmd;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.nutz.http.Http;
 import org.nutz.http.Response;
 import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.lang.Strings;
 import org.nutz.walnut.api.err.Er;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.api.io.WnRace;
 import org.nutz.walnut.impl.box.JvmExecutor;
 import org.nutz.walnut.impl.box.WnSystem;
+import org.nutz.walnut.util.Wn;
+import org.nutz.walnut.util.ZParams;
 
 @IocBean
 public class cmd_wget extends JvmExecutor {
 
-	public void exec(WnSystem sys, String[] _args) {
-	    List<String> args = Arrays.asList(_args);
-		if (args.isEmpty()) {
-			throw Er.create("e.cmd.noargs");
-		}
-		String target = null;
-		String url = args.get(0);
-		if (args.size() == 3) {
-			if (!args.get(0).equals("-O")) {
-				throw Er.create("e.cmd.badargs");
-			}
-			target = args.get(1);
-			url = args.get(2);
-		} else {
-			if (url.contains("?"))
-				target = url.substring(url.lastIndexOf("/")+1, url.indexOf("?"));
-			else
-				target = url.substring(url.lastIndexOf("/")+1);
-		}
-		if (!url.startsWith("http")) {
-			url = "http://" + url;
-		}
-		if (target.contains("/")) {
-			sys.out.println("!only support download to current dir");
-			return;
-		}
-		WnObj p = sys.getCurrentObj(); 
-		Response resp = Http.get(url);
-		if (!resp.isOK()) {
-			throw Er.create("e.cmd.badargs");
-		}
-		int sz = resp.getHeader().getInt("Content-Length", -1);
-		if (sz > -1) {
-			sys.out.println("size=" + sz);
-		}
-		WnObj obj = sys.io.createIfNoExists(p, target, WnRace.FILE);
-		sys.io.writeAndClose(obj, resp.getStream());
-		sys.out.println("done");
-	}
+    public void exec(WnSystem sys, String[] args) {
+        // 分析参数
+        ZParams params = ZParams.parse(args, null);
+
+        // 得到目标 URL
+        String url = params.val_check(0);
+
+        // 得到输出文件
+        String phTa = params.get("O");
+        if (Strings.isBlank(phTa)) {
+            int pos = url.lastIndexOf('/');
+            if (pos > 0) {
+                phTa = url.substring(pos + 1);
+            } else {
+                phTa = "index.html";
+            }
+            // 去掉参数
+            pos = phTa.indexOf('?');
+            if (pos > 0) {
+                phTa = phTa.substring(0, pos);
+            }
+        }
+        String aph = Wn.normalizeFullPath(phTa, sys);
+        WnObj oTa = sys.io.createIfNoExists(sys.getCurrentObj(), aph, WnRace.FILE);
+
+        // 发起请求
+        Response resp = Http.get(url);
+        if (!resp.isOK()) {
+            throw Er.create("e.cmd.badargs");
+        }
+        int sz = resp.getHeader().getInt("Content-Length", -1);
+        if (sz > -1) {
+            sys.out.println("size=" + sz);
+        }
+        sys.io.writeAndClose(oTa, resp.getStream());
+        sys.out.println("done");
+    }
 
 }
