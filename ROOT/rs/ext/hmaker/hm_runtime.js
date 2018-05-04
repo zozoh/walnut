e9022473aae2bc08a896b73f2d1761bfa5f18755
Peter Zhang <zozohtnt@gmail.com>
2018-05-05 01:04:27 +0800

    ...

--- ROOT/rs/ext/hmaker/hm_runtime.js
+++ ROOT/rs/ext/hmaker/hm_runtime.js
@@ -29,7 +29,7 @@ window.HmRT = {
         if(!line)
             return;
 
-        console.log(line)
+        //console.log(line)
 
         // 字段
         var m = /^([.>!])([0-9a-zA-Z_|]+)(<(.+)>)?(\[([+-])\])?(:([^=]+))?(=(.+))?$/.exec(line);
