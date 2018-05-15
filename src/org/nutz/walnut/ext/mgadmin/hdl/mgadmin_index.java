d3fbcd54a29023474d7758b0a3bb6ecd7ec43a0e
Wendal Chen <wendal1985@gmail.com>
2018-05-15 19:18:01 +0800

    add: mgadmin index check_pid_nm 命令

--- src/org/nutz/walnut/ext/mgadmin/hdl/mgadmin_index.java
+++ src/org/nutz/walnut/ext/mgadmin/hdl/mgadmin_index.java
@@ -1,13 +1,17 @@
 package org.nutz.walnut.ext.mgadmin.hdl;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.nutz.log.Log;
 import org.nutz.walnut.impl.box.JvmHdl;
 import org.nutz.walnut.impl.box.JvmHdlContext;
 import org.nutz.walnut.impl.box.WnSystem;
 import org.nutz.walnut.impl.io.mongo.MongoDB;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.Cursor;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
@@ -43,10 +47,14 @@ public class mgadmin_index implements JvmHdl {
                         sys.out.println("drop NOT unique id index for collection obj");
                         co.dropIndex("id_1");
                     }
+                    if ("pid_1_nm_1".equals(dbo.get("name")) && dbo.get("unique") != Boolean.TRUE) {
+                        sys.out.println("drop NOT unique pid+nm index for collection obj");
+                        co.dropIndex("pid_1_nm_1");
+                    }
                 }
                 co.createIndex(toDBO("{'id':1}"), toDBO("{'unique':true}"));
                 co.createIndex(toDBO("{'d0':1, 'd1':1}"));
-                co.createIndex(toDBO("{'pid':1, 'nm':1}"));
+                co.createIndex(toDBO("{'pid':1, 'nm':1}"), toDBO("{'unique':true}"));
                 co.createIndex(toDBO("{'www':1}"), toDBO("{'sparse':true}"));
                 co.createIndex(toDBO("{'expi':1}"), toDBO("{'sparse':true}"));
                 co.createIndex(toDBO("{'d0':1, 'd1':1,'www':1}"), toDBO("{'sparse':true}"));
@@ -62,6 +70,25 @@ public class mgadmin_index implements JvmHdl {
                 }
                 co.createIndex(toDBO("{'id':1}"), toDBO("{'unique':true}"));
                 co.createIndex(toDBO("{'sha1':1}"));
+                break;
+            case "check_pid_nm":
+                co = db.getCollection("obj");
+                Set<String> keys = new HashSet<>();
+                Cursor cur = co.find(toDBO("{}"), toDBO("{id:1,pid:1,nm:1}")).batchSize(128);
+                Log log = sys.getLog("debug", null);
+                while (cur.hasNext()) {
+                    DBObject dbo = cur.next();
+                    String pid = (String) dbo.get("pid");
+                    String nm = (String)dbo.get("nm");
+                    if (pid == null || nm == null) {
+                        log.debug("FUCK(pid or nm == null) id=" + dbo.get("id"));
+                        continue;
+                    }
+                    boolean re = keys.add(pid + "," + nm);
+                    if (!re) {
+                        log.debugf("| %s | %s | %s |", pid, nm, dbo.get("id"));
+                    }
+                }
                 break;
             default:
                 sys.err.print("unkown cmd=" + hc.params.val(0));
