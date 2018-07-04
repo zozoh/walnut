4ffbdaffd7c79757b5509a740fe300c99bb6bf80
Wendal Chen <wendal1985@gmail.com>
2018-07-04 19:43:21 +0800

    update: thing_update命令支持safe参数

--- src/org/nutz/walnut/ext/thing/hdl/thing_update.java
+++ src/org/nutz/walnut/ext/thing/hdl/thing_update.java
@@ -1,8 +1,12 @@
 package org.nutz.walnut.ext.thing.hdl;
 
+import java.util.HashSet;
+import java.util.regex.Pattern;
+
 import org.nutz.lang.Lang;
 import org.nutz.lang.Strings;
 import org.nutz.lang.util.NutMap;
+import org.nutz.lang.util.Regex;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.ext.thing.WnThingService;
 import org.nutz.walnut.ext.thing.util.Things;
@@ -33,6 +37,31 @@ public class thing_update implements JvmHdl {
         String th_nm = hc.params.val(1);
         if (!Strings.isBlank(th_nm)) {
             meta.put("th_nm", th_nm);
+        }
+
+        // 看看是否需要safe机制
+        if (hc.params.has("safe")) {
+            NutMap safe = null;
+            // 如果是true,使用thing set的设置项
+            if ("true".equals(hc.params.get("safe"))) {
+                safe = oTs.getAs("th_safe", NutMap.class);
+            }
+            // 否则肯定是个map
+            else {
+                safe = Lang.map(hc.params.get("safe"));
+            }
+            if (safe != null) {
+                String actived = safe.getString("actived");
+                Pattern act = Strings.isBlank(actived) ? null : Regex.getPattern(actived);
+                String locked = safe.getString("locked");
+                Pattern lock = Strings.isBlank(locked) ? null : Regex.getPattern(locked);
+                for (String key : new HashSet<>(meta.keySet())) {
+                    if (act != null && !act.matcher(key).find())
+                        meta.remove(key);
+                    if (lock != null && lock.matcher(key).find())
+                        meta.remove(key);
+                }
+            }
         }
 
         // 准备调用接口
