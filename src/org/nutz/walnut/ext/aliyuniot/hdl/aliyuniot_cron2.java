b53d738a50ec8657a6da3383a3d255cd6d216ea7
Wendal Chen <wendal1985@gmail.com>
2018-09-17 21:46:50 +0800

    add: aliyuniot 添加 on2/off2/cron2 方法

---
+++ src/org/nutz/walnut/ext/aliyuniot/hdl/aliyuniot_cron2.java
@@ -0,0 +1,55 @@
+package org.nutz.walnut.ext.aliyuniot.hdl;
+
+import org.nutz.json.Json;
+import org.nutz.lang.Strings;
+import org.nutz.lang.util.NutMap;
+import org.nutz.walnut.impl.box.JvmHdlContext;
+import org.nutz.walnut.impl.box.JvmHdlParamArgs;
+import org.nutz.walnut.impl.box.WnSystem;
+import org.nutz.web.Webs.Err;
+
+@JvmHdlParamArgs(value="cqn", regex="^dry$")
+public class aliyuniot_cron2 extends aliyuniot_pub {
+
+    @Override
+    public void invoke(WnSystem sys, JvmHdlContext hc) throws Exception {
+        String imei = hc.params.val_check(0);
+        String cron = hc.params.val_check(1);
+        if (Strings.isBlank(imei) || Strings.isBlank(cron)) {
+            throw Err.create("e.cmd.npower_cron.miss_args");
+        }
+        // 校验cron是否合法,模拟lua代码的解析过程
+        if (cron.contains(" ") || cron.contains("\t") || cron.contains("\n")) {
+            throw Err.create("e.cmd.npower_cron.invaild_cron_with_blank");
+        }
+        // on:000000-010000,off:010001-050000,on:050001-235959
+        // 0点到1点开启, 1点零1秒到5点关闭,5点零1秒到23点59分59秒开启
+        for (String tmp : cron.split(",")) {
+            // 不允许空字符串
+            if (Strings.isBlank(tmp)) {
+                throw Err.create("e.cmd.npower_cron.invaild_cron_with_blank");
+            }
+            // 使用冒号分隔
+            String[] tmp2 = tmp.split("\\:");
+            if (tmp2.length != 2) {
+                throw Err.create("e.cmd.npower_cron.invaild_cron_with_blank");
+            }
+            if (!("on".equals(tmp2[0]) || "off".equals(tmp2[0]))) {
+                throw Err.create("e.cmd.npower_cron.only_on_off");
+            }
+            // 第二部分是时间定义,肯定是 6+1+6=13个字符
+            if (tmp2[1].length() != 13) {
+                throw Err.create("e.cmd.npower_cron.invaild_time");
+            }
+            // 接下来,解析是否为数字
+            tmp2 = tmp2[1].split("\\-");
+            Integer.parseInt(tmp2[0], 10);
+            Integer.parseInt(tmp2[1], 10);
+            // 应该就好了
+        }
+        // 看来合法了
+        hc.params.setv("msg", Json.toJson(new NutMap("method", "setv").setv("data", new NutMap("Cron", cron).setv("OpMode", "cron"))));
+        super.invoke(sys, hc);
+    }
+
+}
