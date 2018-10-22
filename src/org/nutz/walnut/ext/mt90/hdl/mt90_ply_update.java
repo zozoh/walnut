0cb6f80d8f4d14c61d3038e98799817a1b8ca20d
Wendal Chen <wendal1985@gmail.com>
2018-10-22 20:53:38 +0800

    add: mt90_ply_update支持更新多个文件,无需重算多次

--- src/org/nutz/walnut/ext/mt90/hdl/mt90_ply_update.java
+++ src/org/nutz/walnut/ext/mt90/hdl/mt90_ply_update.java
@@ -17,7 +17,7 @@ public class mt90_ply_update implements JvmHdl {
     @Override
     public void invoke(WnSystem sys, JvmHdlContext hc) throws Exception {
         String line = sys.io.readText(sys.io.check(null, Wn.normalizeFullPath(hc.params.val_check(0), sys)));
-        WnObj wobj = sys.io.check(null, Wn.normalizeFullPath(hc.params.val_check(1), sys));
+        
         Mt90Raw raw = Mt90Raw.mapping(line.trim());
         if (raw == null) {
             return;
@@ -28,11 +28,8 @@ public class mt90_ply_update implements JvmHdl {
         if (1 == raw.eventKey) {
             // TODO 报警信息
         }
-        // 过滤跟踪时间
-        if (wobj.getLong("u_trk_tm_gps", 0) > raw.timestamp) {
-            // 属于补传数据,跳过
-            return;
-        }
+
+        // 把更新数据准备好
         String conv_from = "wgs84";
         String conv_to = hc.params.get("conv_to", "gcj02");
         AbstraceWoozPoint point = new AbstraceWoozPoint();
@@ -46,6 +43,15 @@ public class mt90_ply_update implements JvmHdl {
         meta.put("u_ele", point.ele);
         meta.put("u_trk_tm", System.currentTimeMillis());
         meta.put("u_trk_tm_gps", raw.timestamp);
-        sys.io.appendMeta(wobj, meta);
+        
+        for (String val : hc.params.vals) {
+            WnObj wobj = sys.io.check(null, Wn.normalizeFullPath(val, sys));
+            // 过滤跟踪时间
+            if (wobj.getLong("u_trk_tm_gps", 0) > raw.timestamp) {
+                // 属于补传数据,跳过
+                continue;
+            }
+            sys.io.appendMeta(wobj, meta);
+        }
     }
 }
