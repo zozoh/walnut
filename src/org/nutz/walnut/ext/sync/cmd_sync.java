de36a1d89db5938ecbd0ffcfb64942bfa3fdb355
Wendal Chen <wendal1985@gmail.com>
2019-10-08 13:55:33 +0800

    add: 添加sync 2local命令, 可导出文件到本地目录

--- src/org/nutz/walnut/ext/sync/cmd_sync.java
+++ src/org/nutz/walnut/ext/sync/cmd_sync.java
@@ -1,17 +1,11 @@
 package org.nutz.walnut.ext.sync;
 
-import org.nutz.walnut.impl.box.JvmExecutor;
-import org.nutz.walnut.impl.box.WnSystem;
+import org.nutz.walnut.impl.box.JvmHdlExecutor;
 
 /**
  * 
  * @author wendal(wendal1985@gmail.com)
  *
  */
-public class cmd_sync extends JvmExecutor {
-
-    public void exec(WnSystem sys, String[] args) throws Exception {
-        
-    }
-
+public class cmd_sync extends JvmHdlExecutor {
 }
