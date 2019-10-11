32a9311834a0beb6efbba99c87d8f18317721c96
Peter Zhang <zozohtnt@gmail.com>
2019-10-12 01:05:10 +0800

    增强 cmd_httpout

--- src/org/nutz/walnut/impl/box/cmd/cmd_httpout.java
+++ src/org/nutz/walnut/impl/box/cmd/cmd_httpout.java
@@ -3,6 +3,7 @@ package org.nutz.walnut.impl.box.cmd;
 import java.io.InputStream;
 import java.io.OutputStream;
 import org.nutz.lang.Encoding;
+import org.nutz.lang.Strings;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.impl.box.JvmExecutor;
 import org.nutz.walnut.impl.box.WnSystem;
@@ -11,6 +12,11 @@ import org.nutz.walnut.util.WnHttpResponse;
 import org.nutz.walnut.util.ZParams;
 
 public class cmd_httpout extends JvmExecutor {
+
+    private static final String DFT_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                                         + " AppleWebKit/537.36 (KHTML, like Gecko)"
+                                         + " Chrome/77.0.3865.90"
+                                         + " Safari/537.36";
 
     @Override
     public void exec(WnSystem sys, String[] args) throws Exception {
@@ -23,30 +29,40 @@ public class cmd_httpout extends JvmExecutor {
         resp.setEtag(params.getString("etag"));
 
         // 准备下载
-        if (params.is("download")) {
-            resp.setUserAgent(params.getString("UserAgent"));
+        String downloadName = params.get("download");
+        if (!Strings.isBlank(downloadName)) {
+            // 指定了特殊的下载名称
+            if (!"true".equals(downloadName)) {
+                resp.setDownloadName(downloadName);
+            }
+            // 因为有下载名，所以要设置一下 UA，以便编码下载名
+            resp.setUserAgent(params.getString("UserAgent", DFT_UA));
+        }
+        
+        // 指定内容类型
+        if(params.has("ct")) {
+            resp.setContentType(params.get("ct"));
         }
 
         // 准备响应体
         String body = params.getString("body");
 
-        // 来源是标准输入
-        if (null == body) {
-            if (params.vals.length > 0) {
-                byte[] buf = params.val(0).getBytes(Encoding.CHARSET_UTF8);
-                resp.prepare(buf);
-            }
-            // 从管道读取，或者从指定的标准输入读取
-            else {
-                InputStream ins = sys.in.getInputStream();
-                resp.prepare(ins, -1);
-            }
-        }
         // 输入来自文件对象
-        else {
+        if (!Strings.isBlank(body) || "true".equals(body)) {
             WnObj wobj = Wn.checkObj(sys, body);
             String range = params.getString("range");
             resp.prepare(sys.io, wobj, range);
+        }
+        // 直接在参数里指定了内容
+        else if (params.vals.length > 0) {
+            String str = Strings.join("\n", params.vals);
+            byte[] buf = str.getBytes(Encoding.CHARSET_UTF8);
+            resp.prepare(buf);
+        }
+        // 从管道读取，或者从指定的标准输入读取
+        else {
+            InputStream ins = sys.in.getInputStream();
+            resp.prepare(ins, -1);
         }
 
         // 输出
--- src/org/nutz/walnut/impl/box/cmd/man/zh_cn/cmd_httpout.md
+++ src/org/nutz/walnut/impl/box/cmd/man/zh_cn/cmd_httpout.md
@@ -1,38 +1,50 @@
 # 命令简介 
 
-    `httpout` 将给定输出输出成标准的 HTTP 响应
-
+`httpout` 将给定输出输出成标准的 HTTP 响应
+    
 # 用法
 
-    httpout xxxx               # 参数就是响应体的内容，如果指定 -body 则会被无视
-       [-headers {..}]         # 一段 JSON 表示响应头
-       [-status 200]           # 指定状态码，默认 200
-       [-status_text xxx]      # 指定状态文本，默认采用标准文本，譬如 200 就是 'OK'
-       [-body /path/to/file]   # 指定了一个文件，将其内容作为响应体
-       [-etag xxx]             # 输出为标准 HTTP 响应，同时根据 etag 判断是否输出 304
-       [-range bytes=0-7183]   # 当输出 HTTP 响应时，分段下载表示，符合 Http头的 Range 规范
-       [-UserAgent xxx]        # 当输出 HTTP 响应时，指定下载客户端的信息
-       [-download false]       # 当输出 HTTP 响应时，开启下载模式，即指定 Content-Disposition
-             
+```bash
+httpout xxxx               # 参数就是响应体的内容，如果指定 -body 则会被无视
+   [-headers {..}]         # 一段 JSON 表示响应头
+   [-status 200]           # 指定状态码，默认 200
+   [-status_text xxx]      # 指定状态文本，默认采用标准文本，譬如 200 就是 'OK'
+   [-body /path/to/file]   # 指定了一个文件，将其内容作为响应体
+   [-etag xxx]             # 输出为标准 HTTP 响应，同时根据 etag 判断是否输出 304
+   [-range bytes=0-7183]   # 当输出 HTTP 响应时，分段下载表示，符合 Http头的 Range 规范
+   [-ct text/plain]        # 指定响应头的 Content-Type，当然  headers 里指定的要更优先
+   [-UserAgent xxx]        # 当输出 HTTP 响应时，指定下载客户端的信息
+                           # 只有 -download 模式才有效，因为需要根据
+                           # 这个信息编码下载目标名称
+   [-download]             # 指定 Content-Disposition 的名称，在这种模式下
+                           # -UserAgent 才会生效
+                           # 当然，如果 -headers 里指定了  Content-Disposition
+                           # 会更加优先
+```
+      
+> 这里需要说明的是，如果 `httpout` 没有有效的 `-body` 且没有指定 `xxx` 这种直接输入的参数，那么它
+> 会尝试从管道或者标准输入读取内容
     
 # 示例
 
-    # 输入某个重定向
-    demo@~$ httpout -status 302 -headers "Location: 'http://nutzam.com'"
-    HTTP/1.1 302 Found
-    X-Power-By: Walnut
-    Location: http://nutzam.com
-    
-    
-    # 指定 Cookie
-    demo@~$ httpout -headers "'Set-Cookie' : 'AID=34cdeeddww'"
-    HTTP/1.1 200 OK
-    X-Power-By: Walnut
-    Set-Cookie: AID=34cdeeddww
-    Content-Length: 0 
-    
-    # 将文件内容输出
-    demo@~$ httpout -body ~/a/b/c/html
-    
-    # 将文件内容输出（管道方式）
-    demo@~$ cat ~/a/b/c/html | httpout
+```bash
+# 输入某个重定向
+demo@~$ httpout -status 302 -headers "Location: 'http://nutzam.com'"
+HTTP/1.1 302 Found
+X-Power-By: Walnut
+Location: http://nutzam.com
+
+
+# 指定 Cookie
+demo@~$ httpout -headers "'Set-Cookie' : 'AID=34cdeeddww'"
+HTTP/1.1 200 OK
+X-Power-By: Walnut
+Set-Cookie: AID=34cdeeddww
+Content-Length: 0 
+
+# 将文件内容输出
+demo@~$ httpout -body ~/a/b/c/html
+
+# 将文件内容输出（管道方式）
+demo@~$ cat ~/a/b/c/html | httpout
+```
