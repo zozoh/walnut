7ed29628b5a54c489daf3360bea23cd79f03a185
Wendal Chen <wendal1985@gmail.com>
2020-03-19 12:18:05 +0800

    fix: XlsxSheetHandler读取的时候也得是XSSFWorkbook

--- src/org/nutz/walnut/ext/sheet/impl/XlsxSheetHandler.java
+++ src/org/nutz/walnut/ext/sheet/impl/XlsxSheetHandler.java
@@ -3,14 +3,13 @@ package org.nutz.walnut.ext.sheet.impl;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class XlsxSheetHandler extends AbstractPoiSheetHandler {
 
     protected Workbook createWorkbook(InputStream ins) throws IOException {
-        return new HSSFWorkbook(ins);
+        return new XSSFWorkbook(ins);
     }
 
     protected Workbook createWorkbook() {
