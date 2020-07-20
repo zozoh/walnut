fcfd46fcde0df151f722a069f01a1e223b50b648
Peter Zhang <zozohtnt@gmail.com>
2020-07-20 23:37:50 +0800

    ...

--- ROOT/jsbin/mp_bind_phone.js
+++ ROOT/jsbin/mp_bind_phone.js
@@ -17,8 +17,13 @@ function _main(params) {
     if (!params.ticket) {
         return sys.exec("ajaxre -cqn", "e.cmd.auth.bind.miss_ticket")
     }
+    // 网站路径
+    var www = params.www || "~/www";
+    //sys.out.printlnf("www:%s", www);
     var _tmp;
-    var reJSON = sys.exec2("www checkme -cqn ~/www " + sys.safe(params.ticket))
+    var reJSON = sys.exec2("www checkme -cqn '"+www+"' " + sys.safe(params.ticket))
+
+    //sys.out.printlnf("\nreJSON:%s", reJSON);
     _tmp = JSON.parse(reJSON)
     if (!_tmp.me && !_tmp.ok) {
         return sys.out.print(reJSON)
@@ -26,14 +31,16 @@ function _main(params) {
     var me = _tmp.me
     reJSON = sys.exec2("obj -cqn id:" + _tmp.id)
     var session_key = JSON.parse(reJSON).mp_session_key
-    
+    //sys.out.printlnf("\nsession_key:%s", session_key);
     reJSON = sys.exec2f("aes decode -cipher 'AES/CBC/PKCS5PADDING' -iv %s -aeskey %s %s | json -cqn", params.iv, session_key, params.encryptedData)
+
+    //sys.out.printlnf("\naes decode:%s", reJSON);
     log.info("reJSON=" + reJSON)
     var data = JSON.parse(reJSON)
     if (data.phoneNumber) {
        sys.exec2f("obj -u '%s' id:%s", JSON.stringify({phone:data.phoneNumber}), me.id)
     }
     // 业务结束, 生成响应
-    sys.exec("ajaxre -cqn", sys.exec2("www checkme -cqn ~/www " + sys.safe(params.ticket)))
+    sys.exec("ajaxre -cqn", sys.exec2("www checkme -cqn '"+www+"' " + sys.safe(params.ticket)))
 }
 _main(_paramObj)
