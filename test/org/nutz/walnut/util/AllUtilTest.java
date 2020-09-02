f016203fe12544cdc296d19ce359b01aa5f23a0b
Peter Zhang <zozohtnt@gmail.com>
2020-09-02 10:40:02 +0800

    加入了 Junit

--- test/org/nutz/walnut/util/AllUtilTest.java
+++ test/org/nutz/walnut/util/AllUtilTest.java
@@ -2,9 +2,11 @@ package org.nutz.walnut.util;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.nutz.walnut.util.upload.AllUploadTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({CmdsTest.class,
+                     AllUploadTest.class,
                      WnTest.class,
                      WnRgTest.class,
                      ZParamsTest.class,
