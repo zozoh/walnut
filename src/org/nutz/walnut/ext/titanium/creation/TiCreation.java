ef9cd60cc60e29f347da3e64094cdc539b5696e8
Peter Zhang <zozohtnt@gmail.com>
2020-09-28 03:25:42 +0800

    ...

--- src/org/nutz/walnut/ext/titanium/creation/TiCreation.java
+++ src/org/nutz/walnut/ext/titanium/creation/TiCreation.java
@@ -1,88 +1,54 @@
 package org.nutz.walnut.ext.titanium.creation;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class TiCreation {
 
-    private Map<String, String[]> mapping;
+    private boolean freeCreate;
 
-    // 键值为 zh-cn / en-us ...
-    private Map<String, TiTypes> typesByLang;
+    private String[] typeNames;
 
-    public TiCreation() {
-        this.mapping = new HashMap<>();
-        this.typesByLang = new HashMap<>();
+    private List<TiCreateType> types;
+
+    public boolean isFreeCreate() {
+        return freeCreate;
     }
 
-    public void addMapping(Map<String, String[]> mapping) {
-        this.mapping.putAll(mapping);
+    public void setFreeCreate(boolean freeCreate) {
+        this.freeCreate = freeCreate;
     }
 
-    public void addAllTypes(Map<String, TiTypes> typesMap) {
-        for (Map.Entry<String, TiTypes> en : typesMap.entrySet()) {
-            String lang = en.getKey();
-            TiTypes tpmap = en.getValue();
-            this.addTypes(lang, tpmap);
-        }
+    public boolean hasTypeNames() {
+        return null != typeNames && typeNames.length > 0;
     }
 
-    public void addTypes(String lang, TiTypes types) {
-        if (null == types || types.isEmpty())
-            return;
-
-        if (typesByLang.containsKey(lang)) {
-            typesByLang.get(lang).putAll(types);
-        }
-        // 否则新建一个
-        else {
-            typesByLang.put(lang, types.clone());
-        }
+    public String[] getTypeNames() {
+        return typeNames;
     }
 
-    public void mergeWith(TiCreation tic) {
-        this.addMapping(tic.mapping);
-        this.addAllTypes(tic.typesByLang);
+    public void setTypeNames(String[] typeNames) {
+        this.typeNames = typeNames;
     }
 
-    public TiCreationOutput getOutput(String type, String lang) {
-        // 获取映射列表
-        String[] tpNames = mapping.get(type);
-
-        // 获取类型集合
-        TiTypes types = this.typesByLang.get(lang);
-
-        // 准备返回数据
-        TiCreationOutput output = new TiCreationOutput();
-
-        // 组装返回数据
-        // 无
-        if (null == tpNames) {
-            output.asNone();
-        }
-        // 全部类型
-        else if (tpNames.length == 0) {
-            output.setFreeCreate(true);
-            output.asList(types.size());
-            output.addAllTypes(types.values());
-        }
-        // 选取指定类型
-        else {
-            output.asList(tpNames.length);
-            for (String typeName : tpNames) {
-                if ("*".equals(typeName)) {
-                    output.setFreeCreate(true);
-                    continue;
-                }
-                TiTypeInfo info = types.get(typeName);
-                if (null != info) {
-                    output.addType(info);
-                }
+    public void loadTypes(Map<String, TiCreateType> dict) {
+        List<TiCreateType> list = new ArrayList<>(typeNames.length);
+        for (String typeName : typeNames) {
+            TiCreateType tct = dict.get(typeName);
+            if (null != tct) {
+                list.add(tct);
             }
         }
+        this.types = list;
+    }
 
-        // 返回数据
-        return output;
+    public List<TiCreateType> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<TiCreateType> types) {
+        this.types = types;
     }
 
 }
--- src/org/nutz/walnut/ext/titanium/hdl/ti_creation.java
+++ src/org/nutz/walnut/ext/titanium/hdl/ti_creation.java
@@ -1,13 +1,13 @@
 package org.nutz.walnut.ext.titanium.hdl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.nutz.json.Json;
-import org.nutz.lang.Stopwatch;
 import org.nutz.lang.Strings;
-import org.nutz.log.Log;
-import org.nutz.log.Logs;
 import org.nutz.walnut.api.io.WnObj;
+import org.nutz.walnut.ext.titanium.creation.TiCreateType;
 import org.nutz.walnut.ext.titanium.creation.TiCreation;
-import org.nutz.walnut.ext.titanium.creation.TiCreationOutput;
 import org.nutz.walnut.ext.titanium.creation.TiCreationService;
 import org.nutz.walnut.impl.box.JvmHdl;
 import org.nutz.walnut.impl.box.JvmHdlContext;
@@ -18,89 +18,86 @@ import org.nutz.walnut.util.Wn;
 @JvmHdlParamArgs("cqn")
 public class ti_creation implements JvmHdl {
 
-    private static Log log = Logs.get();
-
     private static TiCreationService creations;
 
     @Override
     public void invoke(WnSystem sys, JvmHdlContext hc) throws Exception {
-        try {
-            Stopwatch sw = Stopwatch.begin();
-            // 初始化服务类
-            if (null == creations) {
-                synchronized (ti_views.class) {
-                    if (null == creations) {
-                        creations = hc.ioc.get(TiCreationService.class);
-                    }
+        // 初始化服务类
+        if (null == creations) {
+            synchronized (ti_views.class) {
+                if (null == creations) {
+                    creations = hc.ioc.get(TiCreationService.class);
                 }
             }
-            sw.tag("ok:init-creations");
+        }
 
+        // 获取映射文件名
+        String mappFileName = hc.params.get("m", "creation.json");
+
+        // 获取视图搜寻路径
+        String VIEW_PATH = sys.session.getVars().getString("VIEW_PATH", "/rs/ti/view/");
+        String[] viewHomePaths = Strings.splitIgnoreBlank(VIEW_PATH, ":");
+
+        // 准备获取的创建
+        TiCreation creation = null;
+
+        // 直接指明
+        String viewName = hc.params.get("name");
+        if (!Strings.isBlank(viewName)) {
+            creation = creations.getView(viewName, viewHomePaths);
+        }
+
+        // 如果没有，继续尝试
+        if (null == creation) {
             // 获取要操作的对象
             String aph = hc.params.val_check(0);
             WnObj o = Wn.checkObj(sys, aph);
-            sw.tag("ok:checkObj");
 
-            // 获取视图搜寻路径
-            String VIEW_PATH = sys.session.getVars().getString("VIEW_PATH", "/rs/ti/view/");
-            String[] viewHomePaths = Strings.splitIgnoreBlank(VIEW_PATH, ":");
-
-            // 准备获取的创建
-            TiCreation creation = null;
+            // 在对象里做了声明
+            viewName = o.getString("creation");
+            if (!Strings.isBlank(viewName)) {
+                creation = creations.getView(viewName, viewHomePaths);
+            }
 
             // 读取映射文件
-            for (String viewHomePath : viewHomePaths) {
-                String phCreation = Wn.appendPath(viewHomePath, "creation.json");
-                String aphCreation = Wn.normalizeFullPath(phCreation, sys);
-                WnObj oCreation = sys.io.fetch(null, aphCreation);
-                if (null == oCreation)
-                    continue;
-                sw.tagf("ok:(%s):oCreation", oCreation.path());
-                creation = creations.getCreation(oCreation);
-                if (null != creation) {
-                    sw.tagf("ok:(%s):loaded", oCreation.path());
-                    break;
-                }
-            }
-
-            // 没有找到文件
             if (null == creation) {
-                sw.tagf("fail:VIEW_PATH=%s", VIEW_PATH);
-                sw.stop();
-                sys.out.println("{}");
-            }
-            // 找到了，继续
-            else {
-                // 准备 lang
-                String lang = hc.params.get("lang", "zh-cn");
-
-                // 得到输出
-                String type = "----no-type---";
-                // 指定了类型
-                if (o.hasType()) {
-                    type = o.type();
+                for (String viewHomePath : viewHomePaths) {
+                    String phMapping = Wn.appendPath(viewHomePath, mappFileName);
+                    String aphMapping = Wn.normalizeFullPath(phMapping, sys);
+                    WnObj oMapping = sys.io.fetch(null, aphMapping);
+                    if (null == oMapping)
+                        continue;
+                    creation = creations.getView(oMapping, o, viewHomePaths);
+                    if (null != creation) {
+                        break;
+                    }
                 }
-                // 默认目录用 folder
-                else if (o.isDIR()) {
-                    type = "folder";
-                }
-                TiCreationOutput tcout = creation.getOutput(type, lang);
-                sw.tag("ok:creation.getOutput");
-
-                // 输出
-                String json = Json.toJson(tcout, hc.jfmt);
-                sys.out.println(json);
-                sw.tag("ok:done for ti_creation");
-            }
-            sw.stop();
-            if (log.isDebugEnabled()) {
-                log.debugf("ti_creation done: %s", sw.toString());
             }
         }
-        catch (Exception e) {
-            log.warn("ERR_cmd_ti_creation", e);
-            throw e;
+
+        // 收集类型列表
+        if (null != creation && creation.hasTypeNames()) {
+            Map<String, TiCreateType> typeDict = new HashMap<>();
+            for (String viewHomePath : viewHomePaths) {
+                String phTypes = Wn.appendPath(viewHomePath, "types.json");
+                String aphTypes = Wn.normalizeFullPath(phTypes, sys);
+                WnObj oTypes = sys.io.fetch(null, aphTypes);
+                if (null == oTypes)
+                    continue;
+                TiCreateType[] types = sys.io.readJson(oTypes, TiCreateType[].class);
+                if (null != types) {
+                    for (TiCreateType type : types) {
+                        typeDict.put(type.getName(), type);
+                    }
+                }
+            }
+            creation.loadTypes(typeDict);
         }
+
+        // 输出
+        String json = Json.toJson(creation, hc.jfmt);
+        sys.out.println(json);
+
     }
 
 }
