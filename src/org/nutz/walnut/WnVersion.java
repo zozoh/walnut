ae97f73921a94d582e2de628bc7e57844b6a13b0
Peter Zhang <zozohtnt@gmail.com>
2020-10-10 23:29:22 +0800

    ...

--- src/org/nutz/walnut/WnVersion.java
+++ src/org/nutz/walnut/WnVersion.java
@@ -11,7 +11,7 @@ public final class WnVersion {
      * @return 版本号
      */
     public static String get() {
-        return "3.5";
+        return "3.6";
     }
 
     /**
--- src/org/nutz/walnut/web/module/AppModule.java
+++ src/org/nutz/walnut/web/module/AppModule.java
@@ -2,7 +2,6 @@ package org.nutz.walnut.web.module;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
