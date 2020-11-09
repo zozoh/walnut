02ea66852f4594823f2b23cf8542bf5ef5e52986
Peter Zhang <zozohtnt@gmail.com>
2020-11-09 15:48:19 +0800

    ...

--- src/org/nutz/walnut/ext/titanium/hdl/ti_config.java
+++ src/org/nutz/walnut/ext/titanium/hdl/ti_config.java
@@ -1,6 +1,7 @@
 package org.nutz.walnut.ext.titanium.hdl;
 
 import org.nutz.json.Json;
+import org.nutz.lang.Files;
 import org.nutz.lang.util.NutMap;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.impl.box.JvmHdl;
@@ -11,6 +12,13 @@ import org.nutz.walnut.util.Wn;
 
 @JvmHdlParamArgs("cqn")
 public class ti_config implements JvmHdl {
+
+    private static final NutMap dftDictionaries;
+
+    static {
+        dftDictionaries = Json.fromJsonFile(NutMap.class,
+                                            Files.findFile("org/nutz/walnut/ext/titanium/hdl/wn-dft-dictionaries.json"));
+    }
 
     @Override
     public void invoke(WnSystem sys, JvmHdlContext hc) throws Exception {
@@ -23,6 +31,16 @@ public class ti_config implements JvmHdl {
         // 解析
         else {
             NutMap map = sys.io.readJson(oConfig, NutMap.class);
+
+            // 加入系统默认的字典
+            NutMap dicts = map.getAs("dictionary", NutMap.class);
+            if (null == dicts) {
+                dicts = new NutMap();
+                map.put("dictionary", dicts);
+            }
+            dicts.putAll(dftDictionaries);
+
+            // 输出
             String json = Json.toJson(map, hc.jfmt);
             sys.out.print(json);
         }
