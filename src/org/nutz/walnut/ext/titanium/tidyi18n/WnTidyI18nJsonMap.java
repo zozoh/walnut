2c297d032ed61ad9f3654220f7118c659d76ce73
Peter Zhang <zozohtnt@gmail.com>
2021-01-20 01:29:47 +0800

    将 ti-build/scssimport/i18ntidy/compare 等工具放到 walnut 环境里

---
+++ src/org/nutz/walnut/ext/titanium/tidyi18n/WnTidyI18nJsonMap.java
@@ -0,0 +1,59 @@
+package org.nutz.walnut.ext.titanium.tidyi18n;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.nutz.json.Json;
+import org.nutz.json.JsonFormat;
+import org.nutz.lang.util.NutMap;
+import org.nutz.walnut.api.io.WnIo;
+import org.nutz.walnut.api.io.WnObj;
+
+public class WnTidyI18nJsonMap {
+
+    private WnIo io;
+
+    private WnObj i18nFile;
+
+    public WnTidyI18nJsonMap(WnIo io, WnObj f) {
+        this.io = io;
+        this.i18nFile = f;
+    }
+
+    private NutMap tidyMap(NutMap map) {
+        // 排序
+        List<String> keys = new ArrayList<>(map.size());
+        keys.addAll(map.keySet());
+        Collections.sort(keys);
+
+        // 输出
+        NutMap out = new NutMap();
+        for (String key : keys) {
+            Object val = map.get(key);
+            // if (val instanceof Map) {
+            // NutMap m2 = NutMap.WRAP((Map<String, Object>) val);
+            // val = tidyMap(m2);
+            // }
+            out.put(key, val);
+        }
+
+        // 返回
+        return out;
+    }
+
+    public String doTidy() {
+        // 解析
+        NutMap map = io.readJson(i18nFile, NutMap.class);
+
+        // 排序
+        NutMap out = tidyMap(map);
+
+        // 返回
+        JsonFormat jfmt = JsonFormat.nice();
+        jfmt.setQuoteName(true);
+        jfmt.setIndentBy("  ");
+        return Json.toJson(out, jfmt);
+    }
+
+}
