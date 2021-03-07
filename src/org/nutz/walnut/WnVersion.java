74336f1f739a9a7032c520180e8b46151f99245c
Peter Zhang <zozohtnt@gmail.com>
2021-03-07 23:46:52 +0800

    ...

--- src/org/nutz/walnut/WnVersion.java
+++ src/org/nutz/walnut/WnVersion.java
@@ -11,14 +11,14 @@ public final class WnVersion {
      * @return 版本号
      */
     public static String get() {
-        return "10.24";
+        return "10.25";
     }
 
     /**
      * @return 版本代号
      */
     public static String alias() {
-        return "XAPI";
+        return "Park";
     }
 
     /**
