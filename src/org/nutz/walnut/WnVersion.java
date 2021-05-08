fefca50fb3c8c4546ab6d21a8ecfca23f35d1ace
Peter Zhang <zozohtnt@gmail.com>
2021-05-08 20:35:42 +0800

    update j11

--- src/org/nutz/walnut/WnVersion.java
+++ src/org/nutz/walnut/WnVersion.java
@@ -11,14 +11,14 @@ public final class WnVersion {
      * @return 版本号
      */
     public static String get() {
-        return "10.61";
+        return "10.62";
     }
 
     /**
      * @return 版本代号
      */
     public static String alias() {
-        return "RELOG";
+        return "J11";
     }
 
     /**
