a818a096b32be7ce875d0d838c64dbc9eee76eb4
Peter Zhang <zozohtnt@gmail.com>
2021-05-20 04:04:25 +0800

    ...

--- src/org/nutz/walnut/WnVersion.java
+++ src/org/nutz/walnut/WnVersion.java
@@ -11,14 +11,14 @@ public final class WnVersion {
      * @return 版本号
      */
     public static String get() {
-        return "10.63";
+        return "10.64";
     }
 
     /**
      * @return 版本代号
      */
     public static String alias() {
-        return "WEB";
+        return "AGG";
     }
 
     /**
