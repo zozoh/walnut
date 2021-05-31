bad64b760f025f0ef68a5f47f6d70aef409182b8
Peter Zhang <zozohtnt@gmail.com>
2021-05-31 22:56:31 +0800

    ...

--- src/org/nutz/walnut/ext/data/o/hdl/o_get.java
+++ src/org/nutz/walnut/ext/data/o/hdl/o_get.java
@@ -4,15 +4,19 @@ import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.ext.data.o.OContext;
 import org.nutz.walnut.ext.data.o.OFilter;
 import org.nutz.walnut.impl.box.WnSystem;
+import org.nutz.walnut.util.Ws;
 import org.nutz.walnut.util.ZParams;
 
 public class o_get extends OFilter {
 
     @Override
     protected void process(WnSystem sys, OContext fc, ZParams params) {
-        for (String id : params.vals) {
-            WnObj o = sys.io.checkById(id);
-            fc.add(o);
+        for (String val : params.vals) {
+            String[] ids = Ws.splitIgnoreBlank(val);
+            for (String id : ids) {
+                WnObj o = sys.io.checkById(id);
+                fc.add(o);
+            }
         }
     }
 
