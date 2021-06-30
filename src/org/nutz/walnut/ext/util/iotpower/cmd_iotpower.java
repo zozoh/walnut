e650ba8e838f243c99e46d8ff6adeba30d34d458
Wendal Chen <wendal1985@gmail.com>
2021-06-30 12:40:36 +0800

    add: 添加iotpower激活所需的命令

---
+++ src/org/nutz/walnut/ext/util/iotpower/cmd_iotpower.java
@@ -0,0 +1,7 @@
+package org.nutz.walnut.ext.util.iotpower;
+
+import org.nutz.walnut.impl.box.JvmHdlExecutor;
+
+public class cmd_iotpower extends JvmHdlExecutor {
+
+}
--- src/org/nutz/walnut/ext/util/xxtea/cmd_xxtea.java
+++ src/org/nutz/walnut/ext/util/xxtea/cmd_xxtea.java
@@ -35,4 +35,25 @@ public class cmd_xxtea extends JvmExecutor {
 			sys.out.write(dst);
 	}
 
+	// 实现过程
+	// 1. 上位机会上报 xxx?key=YYYYYY
+	// 其中YYYY的长度为32字节的hex字符串, 如果长度不符, 返回400
+	// 如果YYYY不是合法的字符串, 返回400或者500
+    // 2. 使用 结合 btea和hexbin命令, 解出 dev16的值, 命令如下
+	// echo -n YYYYYY | btea -k "密钥" | hexbin
+	// 3. 根据dev16的值, 在thingset中查询对应的设备, 更新激活时间,并设置新的随机数
+	// 4. 根据新的随机数, 生成新的dev16
+	// 5. 根据新的随机数, 生成激活码, 下发给上位机
+
+	
+	// 小端读取int32
+	static int[] readInt32(byte[] buff, int len) {
+		int[] ret = new int[len];
+		for (int i = 0; i < len; i++) {
+			ret[i] = (buff[i*4+3] & 0xFF) + ((buff[i*4+2] & 0xFF) << 8) 
+				   + ((buff[i*4+1] & 0xFF) << 16) + ((buff[i*4+0] & 0xFF) << 24);
+		}
+		return ret;
+	}
+
 }
