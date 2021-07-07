4c306cd4deb9b3d2f867d9209b205b69d812b6ba
Wendal Chen <wendal1985@gmail.com>
2021-07-07 11:53:08 +0800

    update: 更新iotpower反推函数

--- src/org/nutz/walnut/ext/util/iotpower/hdl/iotpower_activate.java
+++ src/org/nutz/walnut/ext/util/iotpower/hdl/iotpower_activate.java
@@ -51,86 +51,86 @@ public class iotpower_activate implements JvmHdl {
 
 		sys.out.writeJson(ret, JsonFormat.full());
 	}
-//
-//	
-//	public static void main(String[] args) {
-//		String trueUUID = "0072002B0947303032333230";
-//		int random = 0x20210629;
-//		int[] k = BTea.toInt32("Luat IotPower-5V".getBytes());
-//		{
-//			byte[] myUUID = HexBin.decode(trueUUID);
-//			//System.out.println(myUUID.length);
-//			int mU[] = BTea.toInt32(myUUID);
-//			int uuid[] = new int[4];
-//			uuid[0] = (int) ((mU[0] ^ random) - (0x000000FF&(random)));
-//			uuid[1] = (int) ((mU[1] ^ random) - (0x000000FF&(random >> 8)));
-//			uuid[2] = (int) ((mU[2] ^ random) - (0x000000FF&(random >> 16)));
-//			uuid[3] = (int) (random- (0x000000FF&(random >> 24)));
-//
-//			System.out.println("btea加密前");
-//			System.out.printf("%08X\n", uuid[0] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[1] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[2] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[3] & 0xFFFFFFFF);
-//			
-//			byte[] U16 = new byte[16];
-//			for (int i = 0; i < 4; i++) {
-//				U16[i*4+3] = (byte) (uuid[i] & 0xFF);
-//				U16[i*4+2] = (byte) ((uuid[i]>>8) & 0xFF);
-//				U16[i*4+1] = (byte) ((uuid[i]>>16) & 0xFF);
-//				U16[i*4+0] = (byte) ((uuid[i]>>24) & 0xFF);
-//			}
-//			BTea.btea(uuid, 4, k);
-//			
-////			byte[] kkkk = "Luat IotPower-5V".getBytes();
-//			
-////			System.out.printf("key[0] %08X\n", k[0] & 0xFFFFFFFF);
-////			System.out.printf("kkkk[0] %02X%02X%02X%02X\n", kkkk[0] & 0xFF, kkkk[1]& 0xFF, kkkk[2]& 0xFF, kkkk[3]& 0xFF);
-////			System.out.printf("key[0] %08X\n", k[1] & 0xFFFFFFFF);
-////			System.out.printf("key[0] %08X\n", k[2] & 0xFFFFFFFF);
-////			System.out.printf("key[0] %08X\n", k[3] & 0xFFFFFFFF);
-//			
-//			System.out.println("btea加密后");
-//			String dev16 = String.format("%08X%08X%08X%08X", 
-//					uuid[0] & 0xFFFFFFFF, uuid[1] & 0xFFFFFFFF, uuid[2] & 0xFFFFFFFF, uuid[3] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[0] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[1] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[2] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[3] & 0xFFFFFFFF);
-//			System.out.println("QueryString == " + dev16);
-//			
-//			BTea.btea(uuid, -4, k);
-//			
-//			System.out.println("btea解密后");
-//			System.out.printf("%08X\n", uuid[0] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[1] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[2] & 0xFFFFFFFF);
-//			System.out.printf("%08X\n", uuid[3] & 0xFFFFFFFF);
-//			
-//			byte[] crc16 = Modbus.getCrc(BTea.fromInt32(uuid), 16);
-//			
-//			System.out.println("crc16 " + HexBin.encode(crc16));
-//			
-//			int r = 0x12345678;
-//			int rnow = r;
-//			//int w = 0x20212382; int y = 0x20210629;
-//			//System.out.println(HexBin.encode(crc16));
-//			r += ((crc16[1] & 0xFF) << 8) + (crc16[0]&0xFF);
-//			System.out.printf(">>>> %08X%08X\n", r, rnow);
-//			
-//			//System.out.printf("%04X\n", w - y);
-//			
-//			
-//			
-////			int[] v2 = {1,2,3,4};
-////			int[] k2 = {123, 1, 1, 4};
-////			BTea.btea(v2, -4, k2);
-////			System.out.println("错误密码测试---");
-////			System.out.printf("%08X\n", v2[0] & 0xFFFFFFFF);
-////			System.out.printf("%08X\n", v2[1] & 0xFFFFFFFF);
-////			System.out.printf("%08X\n", v2[2] & 0xFFFFFFFF);
-////			System.out.printf("%08X\n", v2[3] & 0xFFFFFFFF);
-//		}
-//		
-//	}
+
+	
+	public static void main(String[] args) {
+		String trueUUID = "0072002B0947303032333230";
+		int random = 0x20210629;
+		int[] k = BTea.toInt32("Luat IotPower-5V".getBytes());
+		{
+			byte[] myUUID = HexBin.decode(trueUUID);
+			//System.out.println(myUUID.length);
+			int mU[] = BTea.toInt32(myUUID);
+			int uuid[] = new int[4];
+			uuid[0] = (int) ((mU[0] ^ random) - (0x000000FF&(random)));
+			uuid[1] = (int) ((mU[1] ^ random) - (0x000000FF&(random >> 8)));
+			uuid[2] = (int) ((mU[2] ^ random) - (0x000000FF&(random >> 16)));
+			uuid[3] = (int) (random- (0x000000FF&(random >> 24)));
+
+			System.out.println("btea加密前");
+			System.out.printf("%08X\n", uuid[0] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[1] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[2] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[3] & 0xFFFFFFFF);
+			
+			byte[] U16 = new byte[16];
+			for (int i = 0; i < 4; i++) {
+				U16[i*4+3] = (byte) (uuid[i] & 0xFF);
+				U16[i*4+2] = (byte) ((uuid[i]>>8) & 0xFF);
+				U16[i*4+1] = (byte) ((uuid[i]>>16) & 0xFF);
+				U16[i*4+0] = (byte) ((uuid[i]>>24) & 0xFF);
+			}
+			BTea.btea(uuid, 4, k);
+			
+//			byte[] kkkk = "Luat IotPower-5V".getBytes();
+			
+//			System.out.printf("key[0] %08X\n", k[0] & 0xFFFFFFFF);
+//			System.out.printf("kkkk[0] %02X%02X%02X%02X\n", kkkk[0] & 0xFF, kkkk[1]& 0xFF, kkkk[2]& 0xFF, kkkk[3]& 0xFF);
+//			System.out.printf("key[0] %08X\n", k[1] & 0xFFFFFFFF);
+//			System.out.printf("key[0] %08X\n", k[2] & 0xFFFFFFFF);
+//			System.out.printf("key[0] %08X\n", k[3] & 0xFFFFFFFF);
+			
+			System.out.println("btea加密后");
+			String dev16 = String.format("%08X%08X%08X%08X", 
+					uuid[0] & 0xFFFFFFFF, uuid[1] & 0xFFFFFFFF, uuid[2] & 0xFFFFFFFF, uuid[3] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[0] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[1] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[2] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[3] & 0xFFFFFFFF);
+			System.out.println("QueryString == " + dev16);
+			
+			BTea.btea(uuid, -4, k);
+			
+			System.out.println("btea解密后");
+			System.out.printf("%08X\n", uuid[0] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[1] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[2] & 0xFFFFFFFF);
+			System.out.printf("%08X\n", uuid[3] & 0xFFFFFFFF);
+			
+			byte[] crc16 = Modbus.getCrc(BTea.fromInt32(uuid), 16);
+			
+			System.out.println("crc16 " + HexBin.encode(crc16));
+			
+			int r = 0x12345678;
+			int rnow = r;
+			//int w = 0x20212382; int y = 0x20210629;
+			//System.out.println(HexBin.encode(crc16));
+			r += ((crc16[1] & 0xFF) << 8) + (crc16[0]&0xFF);
+			System.out.printf(">>>> %08X%08X\n", r, rnow);
+			
+			//System.out.printf("%04X\n", w - y);
+			
+			
+			
+//			int[] v2 = {1,2,3,4};
+//			int[] k2 = {123, 1, 1, 4};
+//			BTea.btea(v2, -4, k2);
+//			System.out.println("错误密码测试---");
+//			System.out.printf("%08X\n", v2[0] & 0xFFFFFFFF);
+//			System.out.printf("%08X\n", v2[1] & 0xFFFFFFFF);
+//			System.out.printf("%08X\n", v2[2] & 0xFFFFFFFF);
+//			System.out.printf("%08X\n", v2[3] & 0xFFFFFFFF);
+		}
+		
+	}
 }
--- src/org/nutz/walnut/ext/util/iotpower/hdl/iotpower_revert.java
+++ src/org/nutz/walnut/ext/util/iotpower/hdl/iotpower_revert.java
@@ -15,28 +15,58 @@ public class iotpower_revert implements JvmHdl {
 		ZParams params = ZParams.parse(hc.args, null);
 
 		String devsecret = params.getString("devsecret"); // 设备id,为24字节长的hex字符串
-		int random = params.getInt("rint", 0x20210629); // 随机数,默认值0x20210629
 
+		byte[] U12 = revert(devsecret);
+
+		sys.out.print(HexBin.encode(U12));
+	}
+	
+	public static byte[] revert(String devsecret) {
+		int random = 0;
 		int[] mU = BTea.toInt32(HexBin.decode(devsecret));
+		{
+			// 反推随机数
+			int lastR = mU[3];
+			int forcheck = 0;
+			for (int i = 0; i < 256; i++) {
+				//System.out.printf("%08X %02X\n", lastR, i);
+				forcheck = lastR + i;
+				if (forcheck >>> 24 == i) {
+					random = forcheck;
+					//System.out.printf("found %02X\n", i);
+					break;
+				}
+			}
+		}
 
 		int uuid[] = new int[3];
 //		uuid[0] = (int) ((mU[0] ^ random) - (0x000000FF & (random)));
-//		uuid[1] = (int) ((mU[1] ^ random) - (0x000000FF & (random >> 8)));
-//		uuid[2] = (int) ((mU[2] ^ random) - (0x000000FF & (random >> 16)));
-		// uuid[3] = (int) (random- (0x000000FF&(random >> 24)));
+//		uuid[1] = (int) ((mU[1] ^ random) - (0x000000FF & (random >>> 8)));
+//		uuid[2] = (int) ((mU[2] ^ random) - (0x000000FF & (random >>> 16)));
+		// uuid[3] = (int) (random- (0x000000FF&(random >>> 24)));
 
 		uuid[0] = (int) ((mU[0] + (0x000000FF & (random)) ^ random));
-		uuid[1] = (int) ((mU[1] + (0x000000FF & (random >> 8)) ^ random));
-		uuid[2] = (int) ((mU[2] + (0x000000FF & (random >> 16)) ^ random));
+		uuid[1] = (int) ((mU[1] + (0x000000FF & (random >>> 8)) ^ random));
+		uuid[2] = (int) ((mU[2] + (0x000000FF & (random >>> 16)) ^ random));
 
 		byte[] U12 = new byte[12];
 		for (int i = 0; i < 3; i++) {
 			U12[i * 4 + 3] = (byte) (uuid[i] & 0xFF);
-			U12[i * 4 + 2] = (byte) ((uuid[i] >> 8) & 0xFF);
-			U12[i * 4 + 1] = (byte) ((uuid[i] >> 16) & 0xFF);
-			U12[i * 4 + 0] = (byte) ((uuid[i] >> 24) & 0xFF);
+			U12[i * 4 + 2] = (byte) ((uuid[i] >>> 8) & 0xFF);
+			U12[i * 4 + 1] = (byte) ((uuid[i] >>> 16) & 0xFF);
+			U12[i * 4 + 0] = (byte) ((uuid[i] >>> 24) & 0xFF);
 		}
-
-		sys.out.print(HexBin.encode(U12));
+		return U12;
+	}
+	
+	public static void main(String[] args) {
+		String cCode = "B591D941A3AE8EF40C7D187D4D1EF032";
+		int[] mU = BTea.toInt32(HexBin.decode(cCode));
+		BTea.btea(mU, -4, BTea.toInt32("Luat IotPower-5V".getBytes()));
+		System.out.println(">> " + HexBin.encode(BTea.fromInt32(mU)));
+		System.out.println(HexBin.encode(revert(HexBin.encode(BTea.fromInt32(mU)))));
+//		System.out.println(HexBin.encode(revert("B591D941A3AE8EF40C7D187D4D1EF032")));
+		// 0072002B0947303032333230
+		// 0072002B0947303032333230
 	}
 }
