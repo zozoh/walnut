480063655638f0b506309c09caf88956cfe839da
Peter Zhang <zozohtnt@gmail.com>
2021-11-25 17:12:48 +0800

    ...

--- src/org/nutz/walnut/ext/data/thing/impl/FileGetAction.java
+++ src/org/nutz/walnut/ext/data/thing/impl/FileGetAction.java
@@ -3,6 +3,7 @@ package org.nutz.walnut.ext.data.thing.impl;
 import org.nutz.walnut.api.err.Er;
 import org.nutz.walnut.api.io.WnObj;
 import org.nutz.walnut.ext.data.thing.ThingDataAction;
+import org.nutz.walnut.util.Wn;
 
 public class FileGetAction extends ThingDataAction<WnObj> {
 
@@ -15,7 +16,13 @@ public class FileGetAction extends ThingDataAction<WnObj> {
         WnObj oDir = this.myDir();
         WnObj oM = io.fetch(oDir, fnm);
         if (null == oM && !quiet) {
-            throw Er.create("e.thing.get." + dirName + ".noexists", oDir.path() + "/" + fnm);
+            String tph;
+            if (null == oDir) {
+                tph = fnm;
+            } else {
+                tph = Wn.appendPath(oDir.path(), fnm);
+            }
+            throw Er.create("e.thing.fileget.noexists", tph);
         }
         return oM;
     }
