b730326b3fa75733bb279370485ce3b68e26d711
Peter Zhang <zozohtnt@gmail.com>
2022-01-06 01:03:20 +0800

    ...

--- src/org/nutz/walnut/WnVersion.java
+++ src/org/nutz/walnut/WnVersion.java
@@ -11,14 +11,14 @@ public final class WnVersion {
      * @return 版本号
      */
     public static String get() {
-        return "11.50";
+        return "11.51";
     }
 
     /**
      * @return 版本代号
      */
     public static String alias() {
-        return "NEW_YEAR";
+        return "TIGER";
     }
 
     /**
