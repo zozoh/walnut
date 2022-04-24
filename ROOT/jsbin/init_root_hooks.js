3bae99942f6eea0da46d4ee86293afde8ea9a408
Peter Zhang <zozohtnt@gmail.com>
2022-04-25 00:05:36 +0800

    ...

--- ROOT/jsbin/init_root_hooks.js
+++ ROOT/jsbin/init_root_hooks.js
@@ -1,6 +1,6 @@
 sys.exec("touch /sys/hook/write/create_thumbnail");
 sys.exec('obj /sys/hook/write/create_thumbnail -u \'hook_by:[{mime:"^image/",ph:"!^/home/.+/(.thumbnail/gen|.publish/gen|www)"}]\'');
-sys.exec('echo \'iimg id:\${id} -thumb 64x64 -Q\' > /sys/hook/write/create_thumbnail');
+sys.exec('echo \'iimg id:\${id} -thumb 256x256 -Q\' > /sys/hook/write/create_thumbnail');
 
 sys.exec("touch /sys/hook/write/create_video_preview");
 sys.exec('obj /sys/hook/write/create_video_preview -u \'hook_by:[{mime:"^video/",ph:"!^/home/.+/(.thumbnail/gen|.publish/gen|www)"}]\'');
