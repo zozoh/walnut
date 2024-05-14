7ed41b58404db4d6c0cfbf4393f594780ed53b2a
Peter Zhang <zozohtnt@gmail.com>
2024-05-15 01:48:24 +0800

    ...

--- src/com/site0/walnut/ext/data/fake/impl/WnDateFormatFaker.java
+++ src/com/site0/walnut/ext/data/fake/impl/WnDateFormatFaker.java
@@ -1,6 +1,8 @@
 package com.site0.walnut.ext.data.fake.impl;
 
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.site0.walnut.ext.data.fake.WnFaker;
 import com.site0.walnut.util.Ws;
@@ -17,9 +19,21 @@ public class WnDateFormatFaker implements WnFaker<String> {
         this.format = Ws.sBlank(format, "yyyy-MM-dd HH:mm:ss.SSS");
     }
 
+    private static Pattern _P = Pattern.compile("^R([0-9]+)$");
+
     @Override
     public String next() {
         long ams = faker.next();
+        if (Ws.isBlank(format)) {
+            return Long.toString(ams);
+        }
+        // By Radix
+        Matcher m = _P.matcher(format);
+        if (m.find()) {
+            int r = Integer.parseInt(m.group(1));
+            return Long.toString(ams, r);
+        }
+        // 字符串形式
         Date d = new Date(ams);
         return Wtime.format(d, format);
     }
