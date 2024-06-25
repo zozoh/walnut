c5d8ddcc4eaafce2b58502f7a35b73e1ca179fdb
Peter Zhang <zozohtnt@gmail.com>
2024-06-26 02:28:35 +0800

    ...

--- src/com/site0/walnut/ext/data/sqlx/ast/cri/SqlCriExpRangeNode.java
+++ src/com/site0/walnut/ext/data/sqlx/ast/cri/SqlCriExpRangeNode.java
@@ -63,56 +63,79 @@ public class SqlCriExpRangeNode extends SqlCriExpressionNode {
 
     private SqlCriExpSimpleNode right;
 
+    private SqlCriExpSimpleEqNode eq;
+
     public SqlCriExpRangeNode(String name, Region<?> range) {
         super(name);
         if (!range.isNull()) {
-            Object l = range.left();
-            if (null != l) {
-                if (range.isLeftOpen()) {
-                    this.left = new SqlCriExpSimpleGtNode(name, l);
-                } else {
-                    this.left = new SqlCriExpSimpleGteNode(name, l);
+            // 区间
+            if (range.isRegion()) {
+                Object l = range.left();
+                if (null != l) {
+                    if (range.isLeftOpen()) {
+                        this.left = new SqlCriExpSimpleGtNode(name, l);
+                    } else {
+                        this.left = new SqlCriExpSimpleGteNode(name, l);
+                    }
+                }
+
+                Object r = range.right();
+                if (null != r) {
+                    if (range.isRightOpen()) {
+                        this.right = new SqlCriExpSimpleLtNode(name, r);
+                    } else {
+                        this.right = new SqlCriExpSimpleLteNode(name, r);
+                    }
                 }
             }
-
-            Object r = range.right();
-            if (null != r) {
-                if (range.isRightOpen()) {
-                    this.right = new SqlCriExpSimpleLtNode(name, r);
-                } else {
-                    this.right = new SqlCriExpSimpleLteNode(name, r);
-                }
+            // 精确匹配
+            else {
+                Object l = range.left();
+                this.eq = new SqlCriExpSimpleEqNode(name, l);
             }
         }
     }
 
     @Override
     protected void _join_self_params(List<SqlParam> params) {
-        if (null != left) {
-            left._join_self_params(params);
+        // 精确匹配
+        if (null != eq) {
+            eq._join_self_params(params);
         }
-        if (null != right) {
-            right._join_self_params(params);
+        // 范围
+        else {
+            if (null != left) {
+                left._join_self_params(params);
+            }
+            if (null != right) {
+                right._join_self_params(params);
+            }
         }
     }
 
     @Override
     protected void _join_self(StringBuilder sb, boolean useParams) {
+        // 精确匹配
+        if (null != eq) {
+            this.eq._join_self(sb, useParams);
+        }
         // 范围
-        if (null != left && null != right) {
-            sb.append("(");
-            left._join_self(sb, useParams);
-            sb.append(" AND ");
-            right._join_self(sb, useParams);
-            sb.append(")");
-        }
-        // 仅左值
-        else if (null != left) {
-            left._join_self(sb, useParams);
-        }
-        // 仅右值
-        else if (null != right) {
-            right._join_self(sb, useParams);
+        else {
+            if (null != left && null != right) {
+                sb.append("(");
+                left._join_self(sb, useParams);
+                sb.append(" AND ");
+                right._join_self(sb, useParams);
+                sb.append(")");
+            }
+            // 仅左值
+            else if (null != left) {
+                left._join_self(sb, useParams);
+            }
+            // 仅右值
+            else if (null != right) {
+                right._join_self(sb, useParams);
+            }
         }
 
     }
