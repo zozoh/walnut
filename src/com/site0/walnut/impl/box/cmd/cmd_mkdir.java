406658eaf324ab18cda70fcdd7021ccdf86df64d
Peter Zhang <zozohtnt@gmail.com>
2024-07-21 01:56:44 +0800

    ...

--- src/com/site0/walnut/impl/box/cmd/cmd_mkdir.java
+++ src/com/site0/walnut/impl/box/cmd/cmd_mkdir.java
@@ -15,42 +15,53 @@ import com.site0.walnut.util.ZParams;
 
 public class cmd_mkdir extends JvmExecutor {
 
-    @Override
-    public void exec(WnSystem sys, String[] args) {
+	@Override
+	public void exec(WnSystem sys, String[] args) {
 
-        ZParams params = ZParams.parse(args, "pocnql");
+		ZParams params = ZParams.parse(args, "pocnql", "^(noexists)$");
 
-        // 至少要有一个值
-        if (params.vals.length == 0) {
-            throw Er.create("e.cmd.mkdir.nopath");
-        }
+		// 至少要有一个值
+		if (params.vals.length == 0) {
+			throw Er.create("e.cmd.mkdir.nopath");
+		}
 
-        // 准备输出
-        List<WnObj> outs = new LinkedList<WnObj>();
+		boolean noexists = params.is("noexists");
 
-        // 得到目标路径
-        for (String ph : params.vals) {
-            String path = Wn.normalizeFullPath(ph, sys);
-            WnObj o;
-            // 如果是创建父
-            if (params.has("p")) {
-                o = sys.io.create(null, path, WnRace.DIR);
-            }
-            // 不能自动创建父
-            else {
-                String pph = Files.getParent(path);
-                WnObj pobj = sys.io.check(null, pph);
-                String nm = Files.getName(path);
-                o = sys.io.create(pobj, nm, WnRace.DIR);
-            }
-            // 计入结果
-            outs.add(o);
-        }
+		// 准备输出
+		List<WnObj> outs = new LinkedList<WnObj>();
 
-        // 输出
-        if (params.is("o")) {
-            Cmds.output_objs(sys, params, null, outs, true);
-        }
-    }
+		// 得到目标路径
+		for (String ph : params.vals) {
+			String path = Wn.normalizeFullPath(ph, sys);
+			WnObj o;
+			// 如果是创建父
+			if (params.has("p")) {
+				if (noexists) {
+					o = sys.io.createIfNoExists(null, path, WnRace.DIR);
+				} else {
+					o = sys.io.create(null, path, WnRace.DIR);
+				}
+
+			}
+			// 不能自动创建父
+			else {
+				String pph = Files.getParent(path);
+				WnObj pobj = sys.io.check(null, pph);
+				String nm = Files.getName(path);
+				if (noexists) {
+					o = sys.io.createIfNoExists(pobj, nm, WnRace.DIR);
+				} else {
+					o = sys.io.create(pobj, nm, WnRace.DIR);
+				}
+			}
+			// 计入结果
+			outs.add(o);
+		}
+
+		// 输出
+		if (params.is("o")) {
+			Cmds.output_objs(sys, params, null, outs, true);
+		}
+	}
 
 }
