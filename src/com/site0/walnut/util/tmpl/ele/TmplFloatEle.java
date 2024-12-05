2c64ec5e991a9c8e4cbf72a54a111257111b93f2
Peter Zhang <zozohtnt@gmail.com>
2024-12-05 12:00:10 +0800

    tmplx float var support DecimalFormat

--- src/com/site0/walnut/util/tmpl/ele/TmplFloatEle.java
+++ src/com/site0/walnut/util/tmpl/ele/TmplFloatEle.java
@@ -1,20 +1,34 @@
 package com.site0.walnut.util.tmpl.ele;
 
+import java.text.DecimalFormat;
+
 import org.nutz.castor.Castors;
 import org.nutz.lang.Strings;
 
 public class TmplFloatEle extends TmplDynamicEle {
 
+    private DecimalFormat decFormat;
+
     public TmplFloatEle(String key, String fmt, String dft) {
         super("float", key, fmt, dft);
-        this.fmt = Strings.sNull(fmt, "%#.2f");
+        // this.fmt = Strings.sNull(fmt, "%#.2f");
+        this.fmt = Strings.sNull(fmt, "0.##");
+        if (this.fmt.indexOf('%') >= 0) {
+            // 采用普通的格式化，那么就不需要 decFormat
+        } else {
+            this.decFormat = new DecimalFormat(this.fmt);
+        }
+
     }
 
     @Override
     protected String _val(Object val) {
         Float n = Castors.me().castTo(val, Float.class);
         if (null != n) {
-            return String.format(fmt, n);
+            if (null != this.decFormat) {
+                return this.decFormat.format(n);
+            }
+            return String.format(this.fmt, n);
         }
         return null;
     }
