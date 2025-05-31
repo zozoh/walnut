4efa6e5bffe325fbf47bd0f1f283c2e1f90ee9d7
Peter Zhang <zozohtnt@gmail.com>
2025-05-31 22:39:11 +0800

    ...

--- src/com/site0/walnut/impl/box/cmd/cmd_str.java
+++ src/com/site0/walnut/impl/box/cmd/cmd_str.java
@@ -11,24 +11,26 @@ public class cmd_str extends JvmExecutor {
 
     @Override
     public void exec(WnSystem sys, String[] args) throws Exception {
-        ZParams params = ZParams.parse(args, "n", "^(trim)$");
+        ZParams params = ZParams.parse(args, "n", "^(trim|replace)$");
 
         // 得到输入
         String str = sys.in.readAll();
 
         // 转换
-        String s2;
+        String s2 = str;
         if (params.has("decodeURI")) {
             String enc = params.getString("decodeURI", "UTF-8");
-            s2 = URLDecoder.decode(str, enc);
+            s2 = URLDecoder.decode(s2, enc);
         }
-        // 去掉空白
-        else if (params.has("trim")) {
-            s2 = Strings.trim(str);
+        // 去掉前后空白
+        if (params.has("trim")) {
+            s2 = Strings.trim(s2);
         }
-        // 默认原样输出
-        else {
-            s2 = str;
+        // 去掉指定字符（正则表达）
+        if (params.has("replace")) {
+            String regex = params.val(0);
+            String targs = params.val(1, "");
+            s2 = s2.replaceAll(regex, targs);
         }
 
         // 输出
--- src/com/site0/walnut/impl/box/cmd/man/zh_cn/cmd_str.md
+++ src/com/site0/walnut/impl/box/cmd/man/zh_cn/cmd_str.md
@@ -10,6 +10,9 @@
 str
     [-decodeURI UTF-8]  # 【选】对字符串解码，默认 UTF-8
     [-trim]             # 【选】去掉字符串左右空白
+    [-replace]          # 【选】替换模式，读取两个路径参数
+                        #  1. -模式-
+                        #  2. -替换- 如果没有就是空字符串
 ```
   
 示例
@@ -23,6 +26,10 @@ Hello
 # URI解码(UTF-8)
 demo:$ echo '"a=%2012"' | str -decodeURI
 a= 12
+
+# 替换字符串
+demo:$ echo '"a=%2012"' | str -replace % '&#37;'
+a=&#37;2012
 ```
     
     
