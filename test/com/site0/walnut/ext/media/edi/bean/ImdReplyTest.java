a68d6c55647ec5485b1d9688f02a51294d867804
jrrx <jrrx@qq.com>
2026-01-12 09:35:44 +0800

    add IMD response loader and test case; update parsing logic

--- test/com/site0/walnut/ext/media/edi/bean/ImdReplyTest.java
+++ test/com/site0/walnut/ext/media/edi/bean/ImdReplyTest.java
@@ -3,11 +3,13 @@ package com.site0.walnut.ext.media.edi.bean;
 import com.site0.walnut.ext.media.edi.loader.EdiMsgs;
 import com.site0.walnut.ext.media.edi.loader.IMDResLoader;
 import com.site0.walnut.ext.media.edi.msg.reply.imd.IcsReplyImdRes;
+import com.site0.walnut.ext.media.edi.msg.reply.imd.ImdReplyHeadErr;
 import org.junit.Test;
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
 import org.nutz.lang.Files;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -27,31 +29,48 @@ public class ImdReplyTest {
         EdiMessage msg = ic.getFirstMessage();
         IMDResLoader loader = EdiMsgs.getIMDResLoader();
         IcsReplyImdRes re = loader.load(msg);
-         System.out.println(Json.toJson(re, JsonFormat.full()));
+        System.out.println(Json.toJson(re, JsonFormat.full()));
 
-//        assertEquals(11, re.getFuncCode());
-//        assertEquals(null, re.getDutyDate());
-//
-//        Map<String, String> statusMap = re.getImdStatus();
-//        assertEquals("CLEAR", statusMap.get("CLEAR"));
-//
-//        // GIS 相关
-//        assertEquals(false, re.isPreLodge());
-//        assertEquals(true, re.isLlb());
-//        assertEquals(true, re.isTlb());
-//
-//        // NAD 相关
-//        assertEquals("FJM396H", re.getMsgRecipient());
-//        assertEquals("AF46JR", re.getBranchId());
-//        assertEquals("5650", re.getBrokerLicNum());
-//        assertEquals("MASTER LOGISTICS PTY LTD", re.getBrokerName());
-//        assertEquals(null, re.getBorkerBoxNum());
-//        assertEquals("THETA WORKS PTY LTD", re.getImporterName());
-//        assertEquals(null, re.getImporterBoxNum());
-//
-//        // RFF 相关
-//        assertEquals("N10", re.getHeaderNatureType());
+        assertEquals(11, re.getFuncCode());
+        assertEquals(null, re.getDutyDate());
 
+        Map<String, String> statusMap = re.getImdStatus();
+        assertEquals("CLEAR", statusMap.get("CLEAR"));
+
+        // GIS 相关
+        assertEquals(false, re.isPreLodge());
+        assertEquals(true, re.isLlb());
+        assertEquals(true, re.isTlb());
+
+        // NAD 相关
+        assertEquals("FJM396H", re.getMsgRecipient());
+        assertEquals("AF46JR", re.getBranchId());
+        assertEquals("5650", re.getBrokerLicNum());
+        assertEquals("MASTER LOGISTICS PTY LTD", re.getBrokerName());
+        assertEquals(null, re.getBorkerBoxNum());
+        assertEquals("THETA WORKS PTY LTD", re.getImporterName());
+        assertEquals(null, re.getImporterBoxNum());
+
+        // RFF 相关
+        assertEquals("N10", re.getHeaderNatureType());
+        assertEquals("B00072552/1/PRD1", re.getRefId());
+        assertEquals("B00072552/1/PRD1".toLowerCase(), re.getRefIdInLower());
+        assertEquals(1, re.getRefVer());
+        assertEquals("TAW23015", re.getImporterRef());
+        assertEquals("AE6L36KNR", re.getImpDecNum());
+        assertEquals("1", re.getImpDecVer());
+        assertEquals("B00072552", re.getBrokerRef());
+
+        // head errs
+        List<ImdReplyHeadErr> headErrs = re.getHeadErrs();
+        assertEquals(1, headErrs.size());
+
+        assertEquals("1", headErrs.get(0).getErrLoc());
+        assertEquals(true, headErrs.get(0).getErrId().indexOf("ID0302") != -1);
+        assertEquals(true, headErrs.get(0).getErrDesc().indexOf("COMMUNITY PROTECTION PERMIT IS APPLICABLE AND REQUIRED") != -1);
+
+
+        List<Map<String, String>> headMoas = re.getHeadMoas();
 
     }
 }
