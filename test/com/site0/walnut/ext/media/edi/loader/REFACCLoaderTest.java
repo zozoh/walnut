0f81c45b848614cb6bb2ebd450a94fbc63d6e909
jrrx <jrrx@qq.com>
2026-02-11 23:14:16 +0800

    add tests for REFACCLoader to validate refund advance EDI message processing

--- test/com/site0/walnut/ext/media/edi/loader/REFACCLoaderTest.java
+++ test/com/site0/walnut/ext/media/edi/loader/REFACCLoaderTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.nutz.json.Json;
 import org.nutz.json.JsonFormat;
 import org.nutz.lang.Files;
+
 import static org.junit.Assert.*;
 
 public class REFACCLoaderTest {
@@ -41,5 +42,52 @@ public class REFACCLoaderTest {
         assertEquals("2", re.getImpDecNumVer());
         assertEquals("B00058894", re.getBrokerRef());
     }
+
+    //refund_res_02.edi.txt
+    @Test
+    public void test_refundRes_02() {
+        String input = _read_input("refund_res_02");
+        EdiInterchange ic = EdiInterchange.parse(input);
+        EdiMessage msg = ic.getFirstMessage();
+        REFACCLoader loader = EdiMsgs.getREFACCLoader();
+        RefundAdv re = loader.load(msg);
+
+        assertNotNull(re);
+        assertEquals(0, re.getRstVer());
+        assertEquals("KM23016-1", re.getDrawbackId());
+        assertEquals("FJM396H", re.getMsgRecipient());
+        assertEquals("AF46JR", re.getBranchId());
+        assertEquals("MASTER LOGISTICS PTY LTD", re.getBrokerName());
+        assertEquals("B00058425/1/PRD1", re.getRefId());
+        assertEquals("b00058425/1/prd1", re.getRefIdInLower());
+        assertEquals(4, re.getRefVer());
+        assertEquals("AE3ATFJAA", re.getImpDecNum());
+        assertEquals("3", re.getImpDecNumVer());
+        assertEquals("B00058425", re.getBrokerRef());
+    }
+
+    //refund_res_03.edi.txt
+    @Test
+    public void test_refundRes_03() {
+        String input = _read_input("refund_res_03");
+        EdiInterchange ic = EdiInterchange.parse(input);
+        EdiMessage msg = ic.getFirstMessage();
+        REFACCLoader loader = EdiMsgs.getREFACCLoader();
+        RefundAdv re = loader.load(msg);
+
+        assertNotNull(re);
+        assertEquals(0, re.getRstVer());
+        assertEquals("ISMEL4159E", re.getDrawbackId());
+        assertEquals("FJM396H", re.getMsgRecipient());
+        assertEquals("AF46JR", re.getBranchId());
+        assertEquals("MASTER LOGISTICS PTY LTD", re.getBrokerName());
+        assertEquals("B00060706/1/PRD1", re.getRefId());
+        assertEquals("b00060706/1/prd1", re.getRefIdInLower());
+        assertEquals(2, re.getRefVer());
+        assertEquals("AE3J3CNXG", re.getImpDecNum());
+        assertEquals("2", re.getImpDecNumVer());
+        assertEquals("B00060706", re.getBrokerRef());
+    }
+
 
 }
