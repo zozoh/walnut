baa1a5a25b9bced28cba487b09204a1115ed94e2
Peter Zhang <zozohtnt@gmail.com>
2026-05-18 02:24:24 +0800

    ...

--- src/com/site0/walnut/ext/sys/lock/hdl/lock_info.java
+++ src/com/site0/walnut/ext/sys/lock/hdl/lock_info.java
@@ -3,6 +3,7 @@ package com.site0.walnut.ext.sys.lock.hdl;
 import com.site0.walnut.ext.sys.lock.LockContext;
 import com.site0.walnut.ext.sys.lock.LockFilter;
 import com.site0.walnut.impl.box.WnSystem;
+import com.site0.walnut.login.usr.WnUser;
 import com.site0.walnut.util.ZParams;
 
 public class lock_info extends LockFilter {
@@ -10,7 +11,10 @@ public class lock_info extends LockFilter {
     @Override
     protected void process(WnSystem sys, LockContext fc, ZParams params) {
         String info = fc.api.getInfo();
-        sys.out.println(info);
+        WnUser me = sys.getMe();
+        String myName = me.getName();
+        String ticket = sys.session.getTicket();
+        sys.out.printlnf("Me=%s; se=%s; Lock=%s", myName, ticket, info);
         fc.quiet = true;
     }
 
