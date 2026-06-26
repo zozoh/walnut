fc2f324e350bb81a154f0fc8519f6494240a5be6
Peter Zhang <zozohtnt@gmail.com>
2026-06-26 16:08:12 +0800

    feat(mailx): 为 mailx_vars 命令添加 -get 参数
    支持从输入变量中提取指定键作为新变量集合，增强变量处理的灵活性。

--- src/com/site0/walnut/ext/net/mailx/hdl/mailx_vars.java
+++ src/com/site0/walnut/ext/net/mailx/hdl/mailx_vars.java
@@ -3,6 +3,8 @@ package com.site0.walnut.ext.net.mailx.hdl;
 import com.site0.walnut.util.Wlang;
 import com.site0.walnut.util.Ws;
 
+import java.util.Map;
+
 import org.nutz.lang.util.NutMap;
 import com.site0.walnut.ext.net.mailx.MailxContext;
 import com.site0.walnut.ext.net.mailx.MailxFilter;
@@ -11,23 +13,36 @@ import com.site0.walnut.util.ZParams;
 
 public class mailx_vars extends MailxFilter {
 
+    @SuppressWarnings("unchecked")
     @Override
     protected void process(WnSystem sys, MailxContext fc, ZParams params) {
+        String getKey = params.getString("get");
+        NutMap vars = new NutMap();
         // 读取标准输入
         if (params.vals.length == 0) {
             String str = sys.in.readAll();
             String json = Ws.trim(str);
             NutMap map = Wlang.map(json);
-            fc.vars.putAll(map);
+            vars.putAll(map);
         }
         // 指定了
         else {
             for (String str : params.vals) {
                 String json = Ws.trim(str);
                 NutMap map = Wlang.map(json);
-                fc.vars.putAll(map);
+                vars.putAll(map);
             }
         }
+
+        if (!Ws.isBlank(getKey)) {
+            Object sub = vars.get(getKey);
+            if (null != sub && (sub instanceof Map<?, ?>)) {
+                vars = NutMap.WRAP((Map<String, Object>) sub);
+            }
+        }
+
+        // 合并到上下文
+        fc.vars.putAll(vars);
     }
 
 }
